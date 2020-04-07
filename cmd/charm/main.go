a50e21a079fd5ac04a9b9bba5d3cb250b2388446
Christian Rocha <christian@rocha.is>
2020-04-07 11:45:30 -0400

    Integrate subcommands with Cobra

--- cmd/charm/main.go
+++ cmd/charm/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -11,7 +10,6 @@ import (
 	"github.com/charmbracelet/charm"
 	"github.com/charmbracelet/charm/ui"
 	"github.com/charmbracelet/charm/ui/common"
-	"github.com/charmbracelet/tea"
 	"github.com/muesli/reflow/indent"
 	"github.com/muesli/reflow/wordwrap"
 	"github.com/spf13/cobra"
@@ -70,123 +68,160 @@ func (th *TermLinkHandler) Error(l *charm.Link) {
 	fmt.Println("Error, something's wrong.")
 }
 
-var ()
+func formatShort(s string) string {
+	return indent.String("\n"+s, indentBy)
+}
 
-func main() {
-	//i := flag.String("i", "", "identity file (ssh key) path")
-	//flag.Parse()
-	cfg, err := charm.ConfigFromEnv()
-	if err != nil {
-		log.Fatal(err)
-	}
-	//if *i != "" {
-	//cfg.SSHKeyPath = *i
-	//cfg.ForceKey = true
-	//}
-	cc, err := charm.NewClient(cfg)
-	if err == charm.ErrMissingSSHAuth {
-		log.Fatal("Missing ssh key. Run `ssh-keygen` to make one or set the `CHARM_SSH_KEY_PATH` env var to your private key path.")
-	}
-	if err != nil {
-		log.Fatal(err)
-	}
+func formatLong(s string) string {
+	return indent.String(wordwrap.String("\n"+s, wrapAt), indentBy)
+}
 
-	rootCmd := &cobra.Command{
+var (
+	identityFile string
+	cfg          *charm.Config
+	cc           *charm.Client
+	err          error
+
+	rootCmd = &cobra.Command{
 		Use:   "charm",
-		Short: indent.String(fmt.Sprintf("\nDo %s stuff", common.Keyword("Charm")), indentBy),
+		Short: formatShort("Do " + common.Keyword("Charm") + " stuff"),
 		Run: func(_ *cobra.Command, _ []string) {
-			if err := tea.UseSysLog("charm-tea"); err != nil {
-				log.Fatal(err)
-			}
+			// Run the TUI
 			if err := ui.NewProgram(cc).Start(); err != nil {
-				log.Fatal(err)
+				fmt.Println(err)
+				os.Exit(1)
 			}
-			return
 		},
 	}
 
-	rootCmd.AddCommand(&cobra.Command{
+	bioCmd = &cobra.Command{
+		Use:    "bio",
+		Hidden: true,
+		Short:  "",
+		Long:   formatLong(""),
+		Run: func(cmd *cobra.Command, args []string) {
+			u, err := cc.Bio()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			fmt.Println(u)
+		},
+	}
+
+	idCmd = &cobra.Command{
+		Use:   "id",
+		Short: "Print your Charm ID",
+		Long:  formatLong("Want to know your " + common.Keyword("Charm ID") + "? You’re in luck, kiddo."),
+		Run: func(cmd *cobra.Command, args []string) {
+			id, err := cc.ID()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			fmt.Println(id)
+		},
+	}
+
+	jwtCmd = &cobra.Command{
 		Use:   "jwt",
 		Short: "Print a JWT token",
-		Long: fmt.Sprintf("\n%s",
-			indent.String(
-				wordwrap.String(common.Keyword("JWT tokens")+" are a way to authenticate to different web services that utilize your Charm account. If you’re a nerd you can use "+common.Code("jwt")+" to get one for yourself.", wrapAt),
-				indentBy,
-			),
-		),
-		Run: func(_ *cobra.Command, _ []string) {
+		Long:  formatLong(common.Keyword("JWT tokens") + " are a way to authenticate to different web services that utilize your Charm account. If you’re a nerd you can use " + common.Code("jwt") + " to get one for yourself."),
+		Run: func(cmd *cobra.Command, args []string) {
 			jwt, err := cc.JWT()
 			if err != nil {
 				log.Fatal(err)
 			}
 			fmt.Printf("%s\n", jwt)
 		},
-	})
+	}
 
-	if err := rootCmd.Execute(); err != nil {
+	keysCmd = &cobra.Command{
+		Use:   "keys",
+		Short: "Print linked keys",
+		Long:  formatLong("Charm accounts are powered by " + common.Keyword("SSH keys") + ". This command prints all of the keys linked to your account. To remove keys use the main " + common.Code("charm") + " interface."),
+		Run: func(cmd *cobra.Command, args []string) {
+			ak, err := cc.AuthorizedKeys()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			fmt.Println(ak)
+		},
+	}
+
+	linkCmd = &cobra.Command{
+		Use:   "link",
+		Short: "Link multiple machines to your Charm account",
+		Long:  formatLong("It’s easy to " + common.Keyword("link") + " multiple machines or keys to your Charm account. Just run " + common.Code("charm link") + " on a machine connected to the account to want to link to start the process."),
+		Run: func(cmd *cobra.Command, args []string) {
+			lh := &TermLinkHandler{}
+			switch len(args) {
+			case 0:
+				err := cc.LinkGen(lh)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+			case 1:
+				err := cc.Link(lh, args[1])
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+			default:
+				fmt.Println("Weird arguments. Have a look at " + common.Code("charm help link") + ".")
+				os.Exit(1)
+			}
+		},
+	}
+
+	nameCmd = &cobra.Command{
+		Use:   "name",
+		Short: "Set your name",
+		Long:  formatLong("Set a " + common.Keyword("name") + " for your account. If the name is already taken, just run it again with a different, cooler name. Basic latin letters and numbers only, and no spaces."),
+		Run: func(cmd *cobra.Command, args []string) {
+			n := args[0]
+			u, err := cc.SetName(n)
+			if err == charm.ErrNameTaken {
+				fmt.Println("User name " + common.Code(n) + " is already taken. Try another, cooler name.")
+				os.Exit(1)
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("@%s ID: %s\n", u.Name, u.CharmID)
+		},
+	}
+)
+
+func main() {
+
+	// Load config
+	cfg, err = charm.ConfigFromEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if identityFile != "" {
+		cfg.SSHKeyPath = identityFile
+		cfg.ForceKey = true
+	}
+
+	// Initialize Charm client
+	cc, err = charm.NewClient(cfg)
+	if err == charm.ErrMissingSSHAuth {
+		log.Fatal("Missing ssh key. Run `ssh-keygen` to make one or set the `CHARM_SSH_KEY_PATH` env var to your private key path.")
+	}
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	return
 
-	args := flag.Args()
-	switch args[0] {
-	case "name":
-		if len(args) != 2 {
-			log.Fatal("Usage: charm name USERNAME")
-		}
-		n := args[1]
-		u, err := cc.SetName(n)
-		if err == charm.ErrNameTaken {
-			fmt.Printf("User name '%s' is already taken. Try another!\n", n)
-			os.Exit(1)
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("@%s ID: %s\n", u.Name, u.CharmID)
-	case "jwt":
-		jwt, err := cc.JWT()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%s", jwt)
-	case "id":
-		id, err := cc.ID()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%s", id)
-	case "bio":
-		u, err := cc.Bio()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%v", u)
-	case "keys":
-		ak, err := cc.AuthorizedKeys()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%s", ak)
-	case "link":
-		lh := &TermLinkHandler{}
-		switch len(args) {
-		case 1:
-			err := cc.LinkGen(lh)
-			if err != nil {
-				log.Fatal(err)
-			}
-		case 2:
-			err := cc.Link(lh, args[1])
-			if err != nil {
-				log.Fatal(err)
-			}
-		default:
-			log.Fatal("Bad link command")
-		}
-	default:
-		fmt.Printf("'%s' is not a valid command", args[0])
+	// Run Cobra
+	rootCmd.PersistentFlags().StringVarP(&identityFile, "identity", "i", "", "path to identity file (that is, an ssh private key)")
+	rootCmd.AddCommand(bioCmd, idCmd, jwtCmd, keysCmd, linkCmd, nameCmd)
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
