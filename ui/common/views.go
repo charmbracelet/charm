b2bfd5def3989f7cd840d5c18bc3a728a1cf476d
Christian Rocha <christian@rocha.is>
2020-04-09 22:33:42 -0400

    Remove some duplicated code

--- ui/common/views.go
+++ ui/common/views.go
@@ -8,24 +8,11 @@ import (
 
 // KeyValueView renders key-value pairs
 func KeyValueView(stuff ...string) string {
-	if len(stuff) == 0 {
-		return ""
-	}
-	var s string
-	for i := 0; i < len(stuff); i++ {
-		if i%2 == 0 {
-			// even
-			s += te.String("│ ").Foreground(Color("241")).String()
-			s += stuff[i] + ": "
-			continue
-		}
-		// odd
-		s += te.String(stuff[i]).Foreground(Color(purpleFg)).String()
-		s += "\n"
-	}
-	return strings.TrimSpace(s)
+	return SelectableKeyValueView(false, stuff...)
 }
 
+// SelectableKeyValueView renders key-value pairs as well as a parameter to
+// mark this view as selected
 func SelectableKeyValueView(selected bool, stuff ...string) string {
 	if len(stuff) == 0 {
 		return ""
