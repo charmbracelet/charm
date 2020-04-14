f6524c299b17c534a4c201da3e98da3b7dd3014d
Christian Rocha <christian@rocha.is>
2020-04-14 19:30:29 -0400

    Standalone version of keygen UI

--- ui/keygen/keygen.go
+++ ui/keygen/keygen.go
@@ -5,8 +5,11 @@ import (
 	"time"
 
 	"github.com/charmbracelet/charm"
+	"github.com/charmbracelet/charm/ui/common"
 	"github.com/charmbracelet/tea"
 	"github.com/charmbracelet/teaparty/spinner"
+	"github.com/muesli/reflow/indent"
+	"github.com/muesli/termenv"
 )
 
 type status int
@@ -28,25 +31,32 @@ type pauseMsg struct{}
 // MODEL
 
 type Model struct {
-	status   status
-	err      error
-	spinner  spinner.Model
-	Finished bool
+	status     status
+	err        error
+	spinner    spinner.Model
+	standalone bool
+	Finished   bool
 }
 
 // INIT
 
 func Init() (tea.Model, tea.Cmd) {
+	m := NewModel()
+	m.standalone = true
+	return m, generateKeys
+}
+
+func NewModel() Model {
 	s := spinner.NewModel()
 	s.Type = spinner.Dot
 	s.ForegroundColor = "241"
-
 	return Model{
-		status:   statusRunning,
-		err:      nil,
-		spinner:  s,
-		Finished: false,
-	}, generateKeys
+		status:     statusRunning,
+		err:        nil,
+		spinner:    s,
+		standalone: false,
+		Finished:   false,
+	}
 }
 
 // UPDATE
@@ -79,6 +89,9 @@ func Update(msg tea.Msg, model tea.Model) (tea.Model, tea.Cmd) {
 		m.spinner, _ = spinner.Update(msg, m.spinner)
 		return m, nil
 	case pauseMsg:
+		if m.standalone {
+			return m, tea.Quit
+		}
 		m.status = statusFinished
 		return m, nil
 	}
@@ -100,11 +113,16 @@ func View(model tea.Model) string {
 	case statusRunning:
 		s += fmt.Sprintf("%s Generating keys...", spinner.View(m.spinner))
 	case statusSuccess:
-		s += "Done!"
+		s += termenv.String("✔").Foreground(common.Color("35")).String()
+		s += "  Done!"
 	case statusError:
 		s += fmt.Sprintf("Uh oh, there's been an error: %v", m.err)
 	case statusQuitting:
 		s += "Exiting..."
+	}
+
+	if m.standalone {
+		return indent.String(fmt.Sprintf("\n%s\n", s), 2)
 	}
 
 	return s
@@ -137,7 +155,7 @@ func Spin(model tea.Model) tea.Sub {
 }
 
 func Pause(model tea.Model) tea.Msg {
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Millisecond * 500)
 	return pauseMsg{}
 }
 
