9f08111b7a615793fdd0a10ca3af4c46eaa7a48c
Toby Padilla <toby@charm.sh>
2020-05-12 16:28:02 -0500

    Refactor Glow api calls, add pagination

--- glow.go
+++ glow.go
@@ -8,15 +8,63 @@ import (
 	"time"
 )
 
-const (
-	apiPrefix = "v1"
-)
-
 type Markdown struct {
 	ID        int        `json:"id"`
 	Note      string     `json:"note"`
 	Body      string     `json:"body,omitempty"`
 	CreatedAt *time.Time `json:"created_at"`
+}
+
+func (cc *Client) GetNews(page int) ([]*Markdown, error) {
+	var news []*Markdown
+	err := cc.makeAPIRequest("GET", fmt.Sprintf("news?page=%d", page), nil, &news)
+	if err != nil {
+		return nil, err
+	}
+	return news, nil
+}
+
+func (cc *Client) GetStash(page int) ([]*Markdown, error) {
+	var stash []*Markdown
+	auth, err := cc.Auth()
+	if err != nil {
+		return nil, err
+	}
+	err = cc.makeAPIRequest("GET", fmt.Sprintf("%s/stash?page=%d", auth.CharmID, page), nil, &stash)
+	if err != nil {
+		return nil, err
+	}
+	return stash, nil
+}
+
+func (cc *Client) GetStashMarkdown(markdownID int) (*Markdown, error) {
+	var md Markdown
+	auth, err := cc.Auth()
+	if err != nil {
+		return nil, err
+	}
+	err = cc.makeAPIRequest("GET", fmt.Sprintf("%s/stash/%d", auth.CharmID, markdownID), nil, &md)
+	if err != nil {
+		return nil, err
+	}
+	return &md, nil
+}
+
+func (cc *Client) StashMarkdown(note string, body string) error {
+	auth, err := cc.Auth()
+	if err != nil {
+		return err
+	}
+	md := &Markdown{Note: note, Body: body}
+	return cc.makeAPIRequest("POST", fmt.Sprintf("%s/stash", auth.CharmID), md, nil)
+}
+
+func (cc *Client) DeleteMarkdown(markdownID int) error {
+	auth, err := cc.Auth()
+	if err != nil {
+		return err
+	}
+	return cc.makeAPIRequest("DELETE", fmt.Sprintf("%s/stash/%d", auth.CharmID, markdownID), nil, nil)
 }
 
 func (cc *Client) authorizeRequest(req *http.Request) error {
@@ -28,106 +76,17 @@ func (cc *Client) authorizeRequest(req *http.Request) error {
 	return nil
 }
 
-func (cc *Client) GetStash() ([]*Markdown, error) {
-	var stash []*Markdown
+func (cc *Client) makeAPIRequest(method string, apiPath string, body interface{}, result interface{}) error {
+	var buf *bytes.Buffer
 	client := &http.Client{}
-	auth, err := cc.Auth()
-	if err != nil {
-		return nil, err
+	url := fmt.Sprintf("%s:%d/v1/%s", cc.config.GlowHost, cc.config.GlowPort, apiPath)
+	if body != nil {
+		buf = &bytes.Buffer{}
+		err := json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return err
+		}
 	}
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s:%d/%s/%s/stash", cc.config.GlowHost, cc.config.GlowPort, apiPrefix, auth.CharmID), nil)
-	if err != nil {
-		return nil, err
-	}
-	if cc.authorizeRequest(req) != nil {
-		return nil, err
-	}
-	req.Header.Set("Content-Type", "application/json")
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("server error")
-	}
-	defer resp.Body.Close()
-	dec := json.NewDecoder(resp.Body)
-	err = dec.Decode(&stash)
-	if err != nil {
-		return nil, err
-	}
-	return stash, nil
-}
-
-func (cc *Client) GetMarkdown(markdownID int) (*Markdown, error) {
-	var md Markdown
-	client := &http.Client{}
-	auth, err := cc.Auth()
-	if err != nil {
-		return nil, err
-	}
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s:%d/%s/%s/stash/%d", cc.config.GlowHost, cc.config.GlowPort, apiPrefix, auth.CharmID, markdownID), nil)
-	if err != nil {
-		return nil, err
-	}
-	if cc.authorizeRequest(req) != nil {
-		return nil, err
-	}
-	req.Header.Set("Content-Type", "application/json")
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("server error")
-	}
-	defer resp.Body.Close()
-	dec := json.NewDecoder(resp.Body)
-	err = dec.Decode(&md)
-	if err != nil {
-		return nil, err
-	}
-	return &md, nil
-}
-
-func (cc *Client) StashMarkdown(note string, body string) error {
-	md := &Markdown{Note: note, Body: body}
-	buf := &bytes.Buffer{}
-	err := json.NewEncoder(buf).Encode(md)
-	if err != nil {
-		return err
-	}
-	client := &http.Client{}
-	auth, err := cc.Auth()
-	if err != nil {
-		return err
-	}
-	req, err := http.NewRequest("POST", fmt.Sprintf("%s:%d/%s/%s/stash", cc.config.GlowHost, cc.config.GlowPort, apiPrefix, auth.CharmID), buf)
-	if err != nil {
-		return err
-	}
-	err = cc.authorizeRequest(req)
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Content-Type", "application/json")
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("server error")
-	}
-	return nil
-}
-
-func (cc *Client) DeleteMarkdown(markdownID int) error {
-	client := &http.Client{}
-	auth, err := cc.Auth()
-	if err != nil {
-		return err
-	}
-	req, err := http.NewRequest("DELETE", fmt.Sprintf("%s:%d/%s/%s/stash/%d", cc.config.GlowHost, cc.config.GlowPort, apiPrefix, auth.CharmID, markdownID), nil)
 	if err != nil {
 		return err
 	}
@@ -140,7 +99,15 @@ func (cc *Client) DeleteMarkdown(markdownID int) error {
 		return err
 	}
 	if resp.StatusCode != 200 {
-		return fmt.Errorf("server error")
+		return fmt.Errorf("http server error %d", resp.StatusCode)
+	}
+	if result != nil {
+		defer resp.Body.Close()
+		dec := json.NewDecoder(resp.Body)
+		err = dec.Decode(result)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
