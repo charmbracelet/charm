83ef5492a37b766df441a58c01ac9f360bf25e6e
Christian Rocha <christian@rocha.is>
2020-08-12 16:28:01 -0400

    Tone down help and add note about root for Bash on Linux

--- cmd/charm/completion.go
+++ cmd/charm/completion.go
@@ -8,38 +8,38 @@ import (
 )
 
 func completionInstructions() string {
-	return formatLong(`Charm supports shell completion for bash, zsh, fish and powershell.
+	return formatLong(`Charm supports ` + common.Keyword("shell completion") + ` for bash, zsh, fish and powershell.
 
 ` + common.Keyword("Bash") + `
 
 To install completions:
 
-Linux:
-` + common.Code("charm completion bash > /etc/bash_completion.d/charm") + `
+Linux (as root):
+$ charm completion bash > /etc/bash_completion.d/charm
 
 MacOS:
-` + common.Code("charm completion bash > /usr/local/etc/bash_completion.d/charm") + `
+$ charm completion bash > /usr/local/etc/bash_completion.d/charm
 
 Or, to just load for the current session:
-` + common.Code("source <(charm completion bash)") + `
+$ source <(charm completion bash)
 
 ` + common.Keyword("Zsh") + `
 
 If shell completion is not already enabled in your environment you will need to enable it. You can execute the following once:
-` + common.Code(`echo "autoload -U compinit; compinit" >> ~/.zshrc`) + `
+$ echo "autoload -U compinit; compinit" >> ~/.zshrc
 
 Then, to install completions:
-` + common.Code(`charm completion zsh > "${fpath[1]}/_charm"`) + `
+$ charm completion zsh > "${fpath[1]}/_charm"
 
 You will need to start a new shell for this setup to take effect.
 
 ` + common.Keyword("Fish") + `
 
 To load completions for each session:
-` + common.Code(`charm completion fish > ~/.config/fish/completions/charm.fish`) + `
+$ charm completion fish > ~/.config/fish/completions/charm.fish
 
 Or to just load in the current session:
-` + common.Code("charm completion fish | source"))
+$ charm completion fish | source`)
 }
 
 var completionCmd = &cobra.Command{
