charmbracelet/charm

commit 029367482d692ec1ca31ecfeb5f8704b6afa0965

Support HTTPS TLS (#7)

server/http.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -10,6 +11,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	charm "github.com/charmbracelet/charm/proto"
 	"github.com/charmbracelet/charm/server/db"
@@ -25,23 +27,22 @@ const resultsPerPage = 50
 
 // HTTPServer is the HTTP server for the Charm Cloud backend.
 type HTTPServer struct {
-	db     db.DB
-	fstore storage.FileStore
-	cfg    *Config
-	mux    *goji.Mux
+	db      db.DB
+	fstore  storage.FileStore
+	cfg     *Config
+	handler http.Handler
+	tlsKey  []byte
+	tlsCert []byte
 }
 
 // NewHTTPServer returns a new *HTTPServer with the specified Config.
 func NewHTTPServer(cfg *Config) (*HTTPServer, error) {
-	// No auth health check endpoint
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "We live!")
-	})
-
 	mux := goji.NewMux()
 	s := &HTTPServer{
-		cfg: cfg,
-		mux: mux,
+		cfg:     cfg,
+		handler: mux,
+		tlsKey:  cfg.TLSKey,
+		tlsCert: cfg.TLSCert,
 	}
 
 	var jwtMiddleware func(http.Handler) http.Handler
@@ -70,15 +71,56 @@ func NewHTTPServer(cfg *Config) (*HTTPServer, error) {
 
 // Start starts the HTTP server on the port specified in the Config.
 func (s *HTTPServer) Start() {
+	useTls := s.cfg.HTTPScheme == "https"
+	tlsCfg := &tls.Config{}
+	listenAddr := fmt.Sprintf(":%d", s.cfg.HTTPPort)
+	server := &http.Server{
+		Addr:      listenAddr,
+		Handler:   s.handler,
+		TLSConfig: tlsCfg,
+	}
+
+	// TLS key and cert files take precedence over passing them as environment variables.
+	if useTls {
+		if len(s.cfg.TLSKey) > 0 && len(s.cfg.TLSCert) > 0 {
+			cert, err := tls.X509KeyPair(s.tlsCert, s.tlsKey)
+			if err != nil {
+				log.Fatalf("couldn't load TLS key pair: %s", err)
+			}
+			tlsCfg.Certificates = []tls.Certificate{cert}
+		} else if s.cfg.TLSKeyFile == "" || s.cfg.TLSCertFile == "" {
+			log.Fatal("TLS key and cert files must be specified if using TLS")
+		}
+	}
+
 	go func() {
-		err := http.ListenAndServe(fmt.Sprintf(":%s", s.cfg.HealthPort), nil)
+		var err error
+		mux := http.NewServeMux()
+		// No auth health check endpoint
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "We live!")
+		})
+
+		hs := &http.Server{
+			Addr:      fmt.Sprintf(":%s", s.cfg.HealthPort),
+			Handler:   mux,
+			TLSConfig: tlsCfg,
+		}
+		if useTls {
+			err = hs.ListenAndServeTLS(s.cfg.TLSCertFile, s.cfg.TLSKeyFile)
+		} else {
+			err = hs.ListenAndServe()
+		}
 		if err != nil {
 			log.Fatalf("http health endpoint server exited with error: %s", err)
 		}
 	}()
-	listenAddr := fmt.Sprintf(":%d", s.cfg.HTTPPort)
-	log.Printf("HTTP server listening on: %s", listenAddr)
-	log.Fatalf("Server crashed: %s", http.ListenAndServe(listenAddr, s.mux))
+
+	log.Printf("%s server listening on: %s", strings.ToUpper(s.cfg.HTTPScheme), listenAddr)
+	log.Fatalf("Server crashed: %s", map[bool]error{
+		true:  server.ListenAndServeTLS(s.cfg.TLSCertFile, s.cfg.TLSKeyFile),
+		false: server.ListenAndServe(),
+	}[useTls])
 }
 
 func (s *HTTPServer) renderError(w http.ResponseWriter) {

server/server.go
@@ -16,18 +16,22 @@ import (
 
 // Config is the configuration for the Charm server.
 type Config struct {
-	Host       string `env:"CHARM_SERVER_HOST" default:"localhost"`
-	SSHPort    int    `env:"CHARM_SERVER_SSH_PORT" default:"35353"`
-	HTTPPort   int    `env:"CHARM_SERVER_HTTP_PORT" default:"35354"`
-	HTTPScheme string `env:"CHARM_SERVER_HTTP_SCHEME" default:"http"`
-	StatsPort  int    `env:"CHARM_SERVER_STATS_PORT" default:"35355"`
-	HealthPort string `env:"CHARM_SERVER_HEALTH_PORT" default:"35356"`
-	DataDir    string `env:"CHARM_SERVER_DATA_DIR" default:"./data"`
-	PublicKey  []byte
-	PrivateKey []byte
-	DB         db.DB
-	FileStore  storage.FileStore
-	Stats      stats.Stats
+	Host        string `env:"CHARM_SERVER_HOST" default:"localhost"`
+	SSHPort     int    `env:"CHARM_SERVER_SSH_PORT" default:"35353"`
+	HTTPPort    int    `env:"CHARM_SERVER_HTTP_PORT" default:"35354"`
+	HTTPScheme  string `env:"CHARM_SERVER_HTTP_SCHEME" default:"http"`
+	StatsPort   int    `env:"CHARM_SERVER_STATS_PORT" default:"35355"`
+	HealthPort  string `env:"CHARM_SERVER_HEALTH_PORT" default:"35356"`
+	DataDir     string `env:"CHARM_SERVER_DATA_DIR" default:"./data"`
+	TLSKey      []byte `env:"CHARM_SERVER_TLS_KEY" default:""`
+	TLSCert     []byte `env:"CHARM_SERVER_TLS_CERT" default:""`
+	TLSKeyFile  string `env:"CHARM_SERVER_TLS_KEY_FILE" default:""`
+	TLSCertFile string `env:"CHARM_SERVER_TLS_CERT_FILE" default:""`
+	PublicKey   []byte
+	PrivateKey  []byte
+	DB          db.DB
+	FileStore   storage.FileStore
+	Stats       stats.Stats
 }
 
 // Server contains the SSH and HTTP servers required to host the Charm Cloud.

commit 7fe5d94385f939f882868c00ccd5f03b1642bc7e

Don't use HTTPS on health endpoint (#8)
Fixes: 029367482d69 ("Support HTTPS TLS (#7)")

server/http.go
@@ -37,6 +37,11 @@ type HTTPServer struct {
 
 // NewHTTPServer returns a new *HTTPServer with the specified Config.
 func NewHTTPServer(cfg *Config) (*HTTPServer, error) {
+	// No auth health check endpoint
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "We live!")
+	})
+
 	mux := goji.NewMux()
 	s := &HTTPServer{
 		cfg:     cfg,
@@ -94,23 +99,7 @@ func (s *HTTPServer) Start() {
 	}
 
 	go func() {
-		var err error
-		mux := http.NewServeMux()
-		// No auth health check endpoint
-		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "We live!")
-		})
-
-		hs := &http.Server{
-			Addr:      fmt.Sprintf(":%s", s.cfg.HealthPort),
-			Handler:   mux,
-			TLSConfig: tlsCfg,
-		}
-		if useTls {
-			err = hs.ListenAndServeTLS(s.cfg.TLSCertFile, s.cfg.TLSKeyFile)
-		} else {
-			err = hs.ListenAndServe()
-		}
+		err := http.ListenAndServe(fmt.Sprintf(":%s", s.cfg.HealthPort), nil)
 		if err != nil {
 			log.Fatalf("http health endpoint server exited with error: %s", err)
 		}