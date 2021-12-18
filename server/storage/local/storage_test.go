e11860e22b72c12eacda04cfe9b2816263a0faf6
Sergio Rubio <rubiojr@frameos.org>
2021-12-18 14:34:43 +0100

    Test subdirs also

--- server/storage/local/storage_test.go
+++ server/storage/local/storage_test.go
@@ -41,7 +41,6 @@ func TestPut(t *testing.T) {
 	if err != nil {
 		t.Fatalf("expected no error when opening file %s", path)
 	}
-	defer file.Close()
 
 	fileInfo, err := file.Stat()
 	if err != nil {
@@ -53,7 +52,44 @@ func TestPut(t *testing.T) {
 	}
 
 	read, err := ioutil.ReadAll(file)
-	if string(read) != "hello world" {
+	if err != nil {
+		t.Fatalf("expected no error when reading file %s", path)
+
+	}
+	if string(read) != content {
 		t.Fatalf("expected content to be %s, got %s", content, string(read))
 	}
+	file.Close()
+
+	content = "bar"
+	path = "/foo/hello.txt"
+	buf = bytes.NewBufferString(content)
+	err = lfs.Put(charmID, path, buf, fs.FileMode(0644))
+	if err != nil {
+		t.Fatalf("expected no error when file path is %s, %v", path, err)
+	}
+
+	file, err = os.Open(filepath.Join(tdir, charmID, path))
+	if err != nil {
+		t.Fatalf("expected no error when opening file %s", path)
+	}
+
+	fileInfo, err = file.Stat()
+	if err != nil {
+		t.Fatalf("expected no error when getting file info for %s", path)
+	}
+
+	if fileInfo.IsDir() {
+		t.Fatalf("expected file %s to be a regular file", path)
+	}
+
+	read, err = ioutil.ReadAll(file)
+	if err != nil {
+		t.Fatalf("expected no error when reading file %s", path)
+
+	}
+	if string(read) != content {
+		t.Fatalf("expected content to be %s, got %s", content, string(read))
+	}
+	file.Close()
 }
