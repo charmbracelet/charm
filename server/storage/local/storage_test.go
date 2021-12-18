bea4bfa20f83ed22eb5a228ef9980a6435503e88
Sergio Rubio <rubiojr@frameos.org>
2021-12-18 14:12:12 +0100

    Return an error if we try to write a file to /
    Fixes #43

---
+++ server/storage/local/storage_test.go
@@ -0,0 +1,61 @@
+package localstorage
+
+import (
+	"bytes"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/uuid"
+)
+
+func TestPut(t *testing.T) {
+	tdir := t.TempDir()
+	charmID := uuid.New().String()
+	buf := bytes.NewBufferString("")
+	lfs, err := NewLocalFileStore(tdir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	paths := []string{"/", "///"}
+	for _, path := range paths {
+		err = lfs.Put(charmID, path, buf, fs.FileMode(0644))
+		if err == nil {
+			t.Fatalf("expected error when file path is %s", path)
+		}
+
+	}
+
+	content := "hello world"
+	path := "/hello.txt"
+	buf = bytes.NewBufferString(content)
+	err = lfs.Put(charmID, path, buf, fs.FileMode(0644))
+	if err != nil {
+		t.Fatalf("expected no error when file path is %s, %v", path, err)
+	}
+
+	file, err := os.Open(filepath.Join(tdir, charmID, path))
+	if err != nil {
+		t.Fatalf("expected no error when opening file %s", path)
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		t.Fatalf("expected no error when getting file info for %s", path)
+	}
+
+	if fileInfo.IsDir() {
+		t.Fatalf("expected file %s to be a regular file", path)
+	}
+
+	fmt.Println(file.Name())
+	read, err := ioutil.ReadAll(file)
+	if string(read) != "hello world" {
+		t.Fatalf("expected content to be %s, got %s", content, string(read))
+	}
+}
