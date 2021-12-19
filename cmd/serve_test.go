363ea11d7c93750a318f8df7bee7a3bc95892f45
Sergio Rubio <rubiojr@frameos.org>
2021-12-19 20:01:12 +0100

    Test database dir exists

--- cmd/serve_test.go
+++ cmd/serve_test.go
@@ -3,34 +3,62 @@ package cmd
 import (
 	"bytes"
 	"context"
-	"fmt"
 	"log"
 	"net"
+	"os"
 	"path/filepath"
 	"regexp"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
+// Thread safe buffer to avoid data races when setting a custom writer
+// for the log
+type Buffer struct {
+	b bytes.Buffer
+	m sync.Mutex
+}
+
+func (b *Buffer) Read(p []byte) (n int, err error) {
+	b.m.Lock()
+	defer b.m.Unlock()
+	return b.b.Read(p)
+}
+
+func (b *Buffer) Write(p []byte) (n int, err error) {
+	b.m.Lock()
+	defer b.m.Unlock()
+	return b.b.Write(p)
+}
+
+func (b *Buffer) String() string {
+	b.m.Lock()
+	defer b.m.Unlock()
+	return b.b.String()
+}
+
 func TestServe(t *testing.T) {
 	tempDir := t.TempDir()
-	out := bytes.NewBufferString("")
-	log.SetOutput(out)
+	buf := Buffer{}
+	log.SetOutput(&buf)
+	defer func() {
+		log.SetOutput(os.Stderr)
+	}()
 	ServeCmd.SetArgs([]string{"--data-dir", tempDir})
 
 	ctx, cancel := context.WithCancel(context.Background())
 	go ServeCmd.ExecuteContext(ctx)
-	defer cancel()
 
 	if !waitForServer(":35354") {
 		assert.FailNow(t, "server did not start")
 	}
+	cancel()
 
-	dbDir := filepath.Join(tempDir, "db")
-	assert.Regexp(t, regexp.MustCompile("HTTP server listening on: :35354"), out.String())
-	assert.Regexp(t, regexp.MustCompile(fmt.Sprintf("Opening SQLite db: %s", dbDir)), out.String())
+	assert.DirExists(t, filepath.Join(tempDir, "db"))
+	assert.Regexp(t, regexp.MustCompile("HTTP server listening on: :35354"), buf.String())
 }
 
 func waitForServer(addr string) bool {
