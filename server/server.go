charmbracelet/charm

commit f1f42fd2391ea797b54cf97d6890e4d584390fc7

Fix database initialization order
Delay database, filestore and stats initialization until the config
values have been set via command line args.

Fixes #35

server/server.go
@@ -49,21 +49,8 @@ func DefaultConfig() *Config {
 	if err != nil {
 		log.Fatalf("could not read environment: %s", err)
 	}
-	dp := fmt.Sprintf("%s/db", cfg.DataDir)
-	err = storage.EnsureDir(dp, 0700)
-	if err != nil {
-		log.Fatalf("could not init sqlite path: %s", err)
-	}
-	db := sqlite.NewDB(dp)
-	fs, err := lfs.NewLocalFileStore(fmt.Sprintf("%s/files", cfg.DataDir))
-	if err != nil {
-		log.Fatalf("could not init file path: %s", err)
-	}
-	sts, err := sls.NewStats(fmt.Sprintf("%s/stats", cfg.DataDir))
-	if err != nil {
-		log.Fatalf("could not init stats db: %s", err)
-	}
-	return cfg.WithDB(db).WithFileStore(fs).WithStats(sts)
+
+	return cfg
 }
 
 // WithDB returns a Config with the provided DB interface implementation.
@@ -94,7 +81,8 @@ func (cfg *Config) WithKeys(publicKey []byte, privateKey []byte) *Config {
 
 // NewServer returns a *Server with the specified Config.
 func NewServer(cfg *Config) (*Server, error) {
-	s := &Server{Config: cfg}
+	s := &Server{}
+	s.init(cfg)
 	ss, err := NewSSHServer(cfg)
 	if err != nil {
 		return nil, err
@@ -114,4 +102,22 @@ func (srv *Server) Start() {
 		srv.http.Start()
 	}()
 	srv.ssh.Start()
+}
+
+func (srv *Server) init(cfg *Config) {
+	dp := fmt.Sprintf("%s/db", cfg.DataDir)
+	err := storage.EnsureDir(dp, 0700)
+	if err != nil {
+		log.Fatalf("could not init sqlite path: %s", err)
+	}
+	db := sqlite.NewDB(dp)
+	fs, err := lfs.NewLocalFileStore(fmt.Sprintf("%s/files", cfg.DataDir))
+	if err != nil {
+		log.Fatalf("could not init file path: %s", err)
+	}
+	sts, err := sls.NewStats(fmt.Sprintf("%s/stats", cfg.DataDir))
+	if err != nil {
+		log.Fatalf("could not init stats db: %s", err)
+	}
+	srv.Config = cfg.WithDB(db).WithFileStore(fs).WithStats(sts)
 }

commit d77f307476a0c6ba6d60f5b76e59c47d4f3d43c8

fix: allow using custom DB, FS and Stats
Check for nils before setting DB, FS, and Stats otherwise init() will
overwrite DB, FS, and Stats if set.

Fixes: f1f42fd2391e ("Fix database initialization order")

server/server.go
@@ -105,19 +105,27 @@ func (srv *Server) Start() {
 }
 
 func (srv *Server) init(cfg *Config) {
-	dp := fmt.Sprintf("%s/db", cfg.DataDir)
-	err := storage.EnsureDir(dp, 0700)
-	if err != nil {
-		log.Fatalf("could not init sqlite path: %s", err)
+	if cfg.DB == nil {
+		dp := fmt.Sprintf("%s/db", cfg.DataDir)
+		err := storage.EnsureDir(dp, 0700)
+		if err != nil {
+			log.Fatalf("could not init sqlite path: %s", err)
+		}
+		db := sqlite.NewDB(dp)
+		srv.Config = cfg.WithDB(db)
 	}
-	db := sqlite.NewDB(dp)
-	fs, err := lfs.NewLocalFileStore(fmt.Sprintf("%s/files", cfg.DataDir))
-	if err != nil {
-		log.Fatalf("could not init file path: %s", err)
+	if cfg.FileStore == nil {
+		fs, err := lfs.NewLocalFileStore(fmt.Sprintf("%s/files", cfg.DataDir))
+		if err != nil {
+			log.Fatalf("could not init file path: %s", err)
+		}
+		srv.Config = cfg.WithFileStore(fs)
 	}
-	sts, err := sls.NewStats(fmt.Sprintf("%s/stats", cfg.DataDir))
-	if err != nil {
-		log.Fatalf("could not init stats db: %s", err)
+	if cfg.Stats == nil {
+		sts, err := sls.NewStats(fmt.Sprintf("%s/stats", cfg.DataDir))
+		if err != nil {
+			log.Fatalf("could not init stats db: %s", err)
+		}
+		srv.Config = cfg.WithStats(sts)
 	}
-	srv.Config = cfg.WithDB(db).WithFileStore(fs).WithStats(sts)
 }