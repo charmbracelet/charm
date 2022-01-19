a8d60463889596bc889245645dba0f214bec0194
Avelino <avelinorun@gmail.com>
2022-01-19 15:21:00 -0300

    clear string formatting in database opening (path) (#53)
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- kv/kv.go
+++ kv/kv.go
@@ -54,7 +54,7 @@ func OpenWithDefaults(name string) (*KV, error) {
 	if err != nil {
 		return nil, err
 	}
-	pn := filepath.Join(fmt.Sprintf("%s/kv/", dd), name)
+	pn := filepath.Join(dd, "/kv/", name)
 	opts := badger.DefaultOptions(pn).WithLoggingLevel(badger.ERROR)
 
 	// By default we have no logger as it will interfere with Bubble Tea
