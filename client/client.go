charmbracelet/charm

commit d42435babe328eded65cfbee0b525d65d1765a10

feat: determine http scheme based on tls config
* Don't expose http scheme as an environment variable
* Set httpScheme based on TLSConfig and TLSFiles in config with a default
value of "http"
* Make health into its own http server
* Use CHARM_SERVER_HOST when appropriate

Signed-off-by: Ayman Bagabas <ayman.bagabas@gmail.com>

server/server.go
@@ -20,22 +20,22 @@ import (
 
 // Config is the configuration for the Charm server.
 type Config struct {
-	Host        string `env:"CHARM_SERVER_HOST" default:"localhost"`
+	Host        string `env:"CHARM_SERVER_HOST"`
 	SSHPort     int    `env:"CHARM_SERVER_SSH_PORT" default:"35353"`
 	HTTPPort    int    `env:"CHARM_SERVER_HTTP_PORT" default:"35354"`
-	HTTPScheme  string `env:"CHARM_SERVER_HTTP_SCHEME" default:"http"`
 	StatsPort   int    `env:"CHARM_SERVER_STATS_PORT" default:"35355"`
 	HealthPort  int    `env:"CHARM_SERVER_HEALTH_PORT" default:"35356"`
 	DataDir     string `env:"CHARM_SERVER_DATA_DIR" default:"./data"`
-	TLSKeyFile  string `env:"CHARM_SERVER_TLS_KEY_FILE" default:""`
-	TLSCertFile string `env:"CHARM_SERVER_TLS_CERT_FILE" default:""`
-	TLSConfig   *tls.Config
+	TLSKeyFile  string `env:"CHARM_SERVER_TLS_KEY_FILE"`
+	TLSCertFile string `env:"CHARM_SERVER_TLS_CERT_FILE"`
 	PublicKey   []byte
 	PrivateKey  []byte
 	DB          db.DB
 	FileStore   storage.FileStore
 	Stats       stats.Stats
+	tlsConfig   *tls.Config
 	jwtKeyPair  JSONWebKeyPair
+	httpScheme  string
 }
 
 // Server contains the SSH and HTTP servers required to host the Charm Cloud.
@@ -48,9 +48,8 @@ type Server struct {
 // DefaultConfig returns a Config with the values populated with the defaults
 // or specified environment variables.
 func DefaultConfig() *Config {
-	cfg := &Config{}
-	err := babyenv.Parse(cfg)
-	if err != nil {
+	cfg := &Config{httpScheme: "http"}
+	if err := babyenv.Parse(cfg); err != nil {
 		log.Fatalf("could not read environment: %s", err)
 	}
 
@@ -83,13 +82,19 @@ func (cfg *Config) WithKeys(publicKey []byte, privateKey []byte) *Config {
 	return cfg
 }
 
+// WithTLSConfig returns a Config with the provided TLS configuration.
+func (cfg *Config) WithTLSConfig(c *tls.Config) *Config {
+	cfg.tlsConfig = c
+	return cfg
+}
+
 func (cfg *Config) httpURL() string {
-	return fmt.Sprintf("%s://%s:%d", cfg.HTTPScheme, cfg.Host, cfg.HTTPPort)
+	return fmt.Sprintf("%s://%s:%d", cfg.httpScheme, cfg.Host, cfg.HTTPPort)
 }
 
 // NewServer returns a *Server with the specified Config.
 func NewServer(cfg *Config) (*Server, error) {
-	s := &Server{}
+	s := &Server{Config: cfg}
 	s.init(cfg)
 
 	pk, err := gossh.ParseRawPrivateKey(cfg.PrivateKey)
@@ -97,6 +102,11 @@ func NewServer(cfg *Config) (*Server, error) {
 		return nil, err
 	}
 	cfg.jwtKeyPair = NewJSONWebKeyPair(pk.(*ed25519.PrivateKey))
+
+	// Use HTTPS when TLS is configured.
+	if cfg.tlsConfig != nil || (cfg.TLSCertFile != "" && cfg.TLSKeyFile != "") {
+		cfg.httpScheme = "https"
+	}
 
 	ss, err := NewSSHServer(cfg)
 	if err != nil {

server/auth.go
@@ -62,7 +62,7 @@ func (me *SSHServer) handleAPIAuth(s ssh.Session) {
 	_ = me.sendJSON(s, charm.Auth{
 		JWT:         j,
 		ID:          u.CharmID,
-		HTTPScheme:  me.config.HTTPScheme,
+		HTTPScheme:  me.config.httpScheme,
 		PublicKey:   u.PublicKey.Key,
 		EncryptKeys: eks,
 	})

server/http.go
@@ -31,6 +31,8 @@ type HTTPServer struct {
 	fstore  storage.FileStore
 	cfg     *Config
 	handler http.Handler
+	server  *http.Server
+	health  *http.Server
 }
 
 type providerJSON struct {
@@ -44,14 +46,21 @@ type providerJSON struct {
 
 // NewHTTPServer returns a new *HTTPServer with the specified Config.
 func NewHTTPServer(cfg *Config) (*HTTPServer, error) {
+	healthMux := http.NewServeMux()
 	// No auth health check endpoint
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	healthMux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "We live!")
-	})
+	}))
+	health := &http.Server{
+		Addr:      fmt.Sprintf("%s:%d", cfg.Host, cfg.HealthPort),
+		Handler:   healthMux,
+		TLSConfig: cfg.tlsConfig,
+	}
 	mux := goji.NewMux()
 	s := &HTTPServer{
 		cfg:     cfg,
 		handler: mux,
+		health:  health,
 	}
 	jwtMiddleware, err := JWTMiddleware(
 		cfg.jwtKeyPair.JWK.Public(),
@@ -61,6 +70,7 @@ func NewHTTPServer(cfg *Config) (*HTTPServer, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	mux.Use(babylogger.Middleware)
 	mux.Use(PublicPrefixesMiddleware([]string{"/v1/public/", "/.well-known/"}))
 	mux.Use(jwtMiddleware)
@@ -86,26 +96,29 @@ func NewHTTPServer(cfg *Config) (*HTTPServer, error) {
 
 // Start starts the HTTP server on the port specified in the Config.
 func (s *HTTPServer) Start() {
-	useTls := s.cfg.HTTPScheme == "https"
-	listenAddr := fmt.Sprintf(":%d", s.cfg.HTTPPort)
-	server := &http.Server{
+	useTLS := s.cfg.httpScheme == "https"
+	listenAddr := fmt.Sprintf("%s:%d", s.cfg.Host, s.cfg.HTTPPort)
+	s.server = &http.Server{
 		Addr:      listenAddr,
 		Handler:   s.handler,
-		TLSConfig: s.cfg.TLSConfig,
+		TLSConfig: s.cfg.tlsConfig,
 	}
 
 	go func() {
-		err := http.ListenAndServe(fmt.Sprintf(":%d", s.cfg.HealthPort), nil)
-		if err != nil {
-			log.Fatalf("http health endpoint server exited with error: %s", err)
+		log.Printf("Starting %s health server on: %s", strings.ToUpper(s.cfg.httpScheme), s.health.Addr)
+		f := "http health endpoint server exited with error: %s"
+		if useTLS {
+			log.Fatalf(f, s.health.ListenAndServeTLS(s.cfg.TLSCertFile, s.cfg.TLSKeyFile))
+		} else {
+			log.Fatal(f, s.health.ListenAndServe())
 		}
 	}()
 
-	log.Printf("%s server listening on: %s", strings.ToUpper(s.cfg.HTTPScheme), listenAddr)
-	if useTls {
-		log.Fatalf("Server crashed: %s", server.ListenAndServeTLS(s.cfg.TLSCertFile, s.cfg.TLSKeyFile))
+	log.Printf("Starting %s server on: %s", strings.ToUpper(s.cfg.httpScheme), listenAddr)
+	if useTLS {
+		log.Fatalf("Server crashed: %s", s.server.ListenAndServeTLS(s.cfg.TLSCertFile, s.cfg.TLSKeyFile))
 	} else {
-		log.Fatalf("Server crashed: %s", server.ListenAndServe())
+		log.Fatalf("Server crashed: %s", s.server.ListenAndServe())
 	}
 }
 

server/middleware.go
@@ -17,6 +17,7 @@ type contextKey string
 
 var ctxUserKey contextKey = "charmUser"
 
+// RequestLimitMiddleware limits the request body size to the specified limit.
 func RequestLimitMiddleware() func(http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {

server/ssh.go
@@ -37,7 +37,7 @@ type SSHServer struct {
 // NewSSHServer creates a new SSHServer from the provided Config.
 func NewSSHServer(cfg *Config) (*SSHServer, error) {
 	s := &SSHServer{config: cfg}
-	addr := fmt.Sprintf(":%d", cfg.SSHPort)
+	addr := fmt.Sprintf("%s:%d", cfg.Host, cfg.SSHPort)
 	b, err := toktok.NewBucket(6)
 	if err != nil {
 		return nil, err

commit 2998b7938850fc73613ce0f6df9571bc37fcebb1

feat: switch to caarlos0/env and set CHARM_SERVER_HOST default value to localhost
Fixes: https://github.com/charmbracelet/charm/issues/78
Fixes: d42435babe32 ("feat: determine http scheme based on tls config")

client/client.go
@@ -13,10 +13,10 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/caarlos0/env/v6"
 	charm "github.com/charmbracelet/charm/proto"
 	"github.com/charmbracelet/keygen"
 	"github.com/golang-jwt/jwt/v4"
-	"github.com/meowgorithm/babyenv"
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/crypto/ssh"
 )
@@ -25,11 +25,11 @@ var nameValidator = regexp.MustCompile("^[a-zA-Z0-9]{1,50}$")
 
 // Config contains the Charm client configuration.
 type Config struct {
-	Host     string `env:"CHARM_HOST" default:"cloud.charm.sh"`
-	SSHPort  int    `env:"CHARM_SSH_PORT" default:"35353"`
-	HTTPPort int    `env:"CHARM_HTTP_PORT" default:"35354"`
-	Debug    bool   `env:"CHARM_DEBUG" default:"false"`
-	Logfile  string `env:"CHARM_LOGFILE" default:""`
+	Host     string `env:"CHARM_HOST" envDefault:"cloud.charm.sh"`
+	SSHPort  int    `env:"CHARM_SSH_PORT" envDefault:"35353"`
+	HTTPPort int    `env:"CHARM_HTTP_PORT" envDefault:"35354"`
+	Debug    bool   `env:"CHARM_DEBUG" envDefault:"false"`
+	Logfile  string `env:"CHARM_LOGFILE" envDefault:""`
 }
 
 // Client is the Charm client.
@@ -48,7 +48,7 @@ type Client struct {
 // ConfigFromEnv loads the configuration from the environment.
 func ConfigFromEnv() (*Config, error) {
 	var cfg Config
-	if err := babyenv.Parse(&cfg); err != nil {
+	if err := env.Parse(&cfg); err != nil {
 		return nil, err
 	}
 	return &cfg, nil

go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/auth0/go-jwt-middleware/v2 v2.0.0-beta
+	github.com/caarlos0/env/v6 v6.9.1
 	github.com/calmh/randomart v1.1.0
 	github.com/charmbracelet/bubbles v0.8.0
 	github.com/charmbracelet/bubbletea v0.19.1
@@ -15,8 +16,7 @@ require (
 	github.com/golang-jwt/jwt/v4 v4.2.0
 	github.com/google/uuid v1.3.0
 	github.com/jacobsa/crypto v0.0.0-20190317225127-9f44e2d11115
-	github.com/mattn/go-isatty v0.0.14-0.20210829144114-504425e14f74
-	github.com/meowgorithm/babyenv v1.3.1
+	github.com/mattn/go-isatty v0.0.14
 	github.com/meowgorithm/babylogger v1.2.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/muesli/go-app-paths v0.2.1

go.sum
@@ -21,6 +21,8 @@ github.com/auth0/go-jwt-middleware/v2 v2.0.0-beta h1:nQQTj7QTef2o7FS0EZqbJZCbyRU
 github.com/auth0/go-jwt-middleware/v2 v2.0.0-beta/go.mod h1:dWL9pw5FgrzT1Hhmt+D0W8XmDDulGHN3yMMQl1Oq4RM=
 github.com/beorn7/perks v0.0.0-20180321164747-3a771d992973/go.mod h1:Dwedo/Wpr24TaqPxmxbtue+5NUziq4I4S80YR8gNf3Q=
 github.com/beorn7/perks v1.0.0/go.mod h1:KWe93zE9D1o94FZ5RNwFwVgaQK1VOXiVxmqh+CedLV8=
+github.com/caarlos0/env/v6 v6.9.1 h1:zOkkjM0F6ltnQ5eBX6IPI41UP/KDGEK7rRPwGCNos8k=
+github.com/caarlos0/env/v6 v6.9.1/go.mod h1:hvp/ryKXKipEkcuYjs9mI4bBCg+UI0Yhgm5Zu0ddvwc=
 github.com/calmh/randomart v1.1.0 h1:evl+iwc10LXtHdMZhzLxmsCQVmWnkXs44SbC6Uk0Il8=
 github.com/calmh/randomart v1.1.0/go.mod h1:DQUbPVyP+7PAs21w/AnfMKG5NioxS3TbZ2F9MSK/jFM=
 github.com/census-instrumentation/opencensus-proto v0.2.1/go.mod h1:f6KPmirojxKA12rnyqOA5BBL4O983OfeGPqjHWSTneU=
@@ -169,8 +171,9 @@ github.com/mattn/go-colorable v0.0.0-20170327083344-ded68f7a9561/go.mod h1:9vuHe
 github.com/mattn/go-isatty v0.0.3/go.mod h1:M+lRXTBqGeGNdLjl/ufCoiOlB5xdOkqRJdNxMWT7Zi4=
 github.com/mattn/go-isatty v0.0.12/go.mod h1:cbi8OIDigv2wuxKPP5vlRcQ1OAZbq2CE4Kysco4FUpU=
 github.com/mattn/go-isatty v0.0.13/go.mod h1:cbi8OIDigv2wuxKPP5vlRcQ1OAZbq2CE4Kysco4FUpU=
-github.com/mattn/go-isatty v0.0.14-0.20210829144114-504425e14f74 h1:3kEhu34+VLPo2YgQ1PXHLQRgMQKtBmq+MmMYEBHGX7U=
 github.com/mattn/go-isatty v0.0.14-0.20210829144114-504425e14f74/go.mod h1:7GGIvUiUoEMVVmxf/4nioHXj79iQHKdU27kJ6hsGG94=
+github.com/mattn/go-isatty v0.0.14 h1:yVuAays6BHfxijgZPzw+3Zlu5yQgKGP2/hcQbHb7S9Y=
+github.com/mattn/go-isatty v0.0.14/go.mod h1:7GGIvUiUoEMVVmxf/4nioHXj79iQHKdU27kJ6hsGG94=
 github.com/mattn/go-runewidth v0.0.9/go.mod h1:H031xJmbD/WCDINGzjvQ9THkh0rPKHF+m2gUSrubnMI=
 github.com/mattn/go-runewidth v0.0.10/go.mod h1:RAqKPSqVFrSLVXbA8x7dzmKdmGzieGRCM46jaSJTDAk=
 github.com/mattn/go-runewidth v0.0.12/go.mod h1:RAqKPSqVFrSLVXbA8x7dzmKdmGzieGRCM46jaSJTDAk=
@@ -179,8 +182,6 @@ github.com/mattn/go-runewidth v0.0.13/go.mod h1:Jdepj2loyihRzMpdS35Xk/zdY8IAYHsh
 github.com/mattn/go-sqlite3 v1.14.9 h1:10HX2Td0ocZpYEjhilsuo6WWtUqttj2Kb0KtD86/KYA=
 github.com/mattn/go-sqlite3 v1.14.9/go.mod h1:NyWgC/yNuGj7Q9rpYnZvas74GogHl5/Z4A/KQRfk6bU=
 github.com/matttproud/golang_protobuf_extensions v1.0.1/go.mod h1:D8He9yQNgCq6Z5Ld7szi9bcBfOoFv/3dc6xSMkL2PC0=
-github.com/meowgorithm/babyenv v1.3.1 h1:18ZEYIgbzoFQfRLF9+lxjRfk/ui6w8U0FWl07CgWvvc=
-github.com/meowgorithm/babyenv v1.3.1/go.mod h1:lwNX+J6AGBFqNrMZ2PTLkM6SO+W4X8DOg9zBDO4j3Ig=
 github.com/meowgorithm/babylogger v1.2.0 h1:lV48OR+bMVR4qVIaUrcTcITGhfV+C/gKnTYCUR++APU=
 github.com/meowgorithm/babylogger v1.2.0/go.mod h1:Kmw1fbhkP4sLJmhiGIpThiG+guQAQ8dQ3GnLa+8Fjf0=
 github.com/mikesmitty/edkey v0.0.0-20170222072505-3356ea4e686a h1:eU8j/ClY2Ty3qdHnn0TyW3ivFoPC/0F1gQZz8yTxbbE=

server/server.go
@@ -9,24 +9,24 @@ import (
 	"log"
 	"path/filepath"
 
+	"github.com/caarlos0/env/v6"
 	"github.com/charmbracelet/charm/server/db"
 	"github.com/charmbracelet/charm/server/db/sqlite"
 	"github.com/charmbracelet/charm/server/stats"
 	sls "github.com/charmbracelet/charm/server/stats/sqlite"
 	"github.com/charmbracelet/charm/server/storage"
 	lfs "github.com/charmbracelet/charm/server/storage/local"
-	"github.com/meowgorithm/babyenv"
 	gossh "golang.org/x/crypto/ssh"
 )
 
 // Config is the configuration for the Charm server.
 type Config struct {
-	Host        string `env:"CHARM_SERVER_HOST"`
-	SSHPort     int    `env:"CHARM_SERVER_SSH_PORT" default:"35353"`
-	HTTPPort    int    `env:"CHARM_SERVER_HTTP_PORT" default:"35354"`
-	StatsPort   int    `env:"CHARM_SERVER_STATS_PORT" default:"35355"`
-	HealthPort  int    `env:"CHARM_SERVER_HEALTH_PORT" default:"35356"`
-	DataDir     string `env:"CHARM_SERVER_DATA_DIR" default:"./data"`
+	Host        string `env:"CHARM_SERVER_HOST" envDefault:"localhost"`
+	SSHPort     int    `env:"CHARM_SERVER_SSH_PORT" envDefault:"35353"`
+	HTTPPort    int    `env:"CHARM_SERVER_HTTP_PORT" envDefault:"35354"`
+	StatsPort   int    `env:"CHARM_SERVER_STATS_PORT" envDefault:"35355"`
+	HealthPort  int    `env:"CHARM_SERVER_HEALTH_PORT" envDefault:"35356"`
+	DataDir     string `env:"CHARM_SERVER_DATA_DIR" envDefault:"./data"`
 	TLSKeyFile  string `env:"CHARM_SERVER_TLS_KEY_FILE"`
 	TLSCertFile string `env:"CHARM_SERVER_TLS_CERT_FILE"`
 	errorLog    *log.Logger
@@ -51,7 +51,7 @@ type Server struct {
 // or specified environment variables.
 func DefaultConfig() *Config {
 	cfg := &Config{httpScheme: "http"}
-	if err := babyenv.Parse(cfg); err != nil {
+	if err := env.Parse(cfg); err != nil {
 		log.Fatalf("could not read environment: %s", err)
 	}
 