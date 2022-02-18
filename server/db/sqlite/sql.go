93b5a7a6877ce8ee9b8591dff8e4e8e991f2b8b1
Ayman Bagabas <ayman.bagabas@gmail.com>
2022-02-18 14:58:41 -0500

    fix: create db tables with fk constraints and cascades
    * Add foreign key constraint
    * Add cascade on update
    * Add cascade on delete

--- server/db/sqlite/sql.go
+++ server/db/sqlite/sql.go
@@ -7,44 +7,71 @@ const (
                         name varchar(50) UNIQUE,
                         email varchar(254),
                         bio varchar(1000),
-                        created_at timestamp default current_timestamp)`
+                        created_at timestamp default current_timestamp
+                        )`
 
 	sqlCreatePublicKeyTable = `CREATE TABLE IF NOT EXISTS public_key(
                              id INTEGER NOT NULL PRIMARY KEY,
-                             user_id integer REFERENCES charm_user (id) NOT NULL,
+                             user_id integer NOT NULL,
                              public_key varchar(2048) NOT NULL,
                              created_at timestamp default current_timestamp,
-                             UNIQUE (user_id, public_key))`
+                             UNIQUE (user_id, public_key),
+                             CONSTRAINT user_id_fk
+                                FOREIGN KEY (user_id)
+                                REFERENCES charm_user (id)
+                                ON DELETE CASCADE
+                                ON UPDATE CASCADE
+                            )`
 
 	sqlCreateEncryptKeyTable = `CREATE TABLE IF NOT EXISTS encrypt_key(
                               id INTEGER NOT NULL PRIMARY KEY,
-                              public_key_id integer REFERENCES public_key (id) ON DELETE CASCADE NOT NULL,
+                              public_key_id integer NOT NULL,
                               global_id uuid NOT NULL,
                               created_at timestamp default current_timestamp,
-                              encrypted_key varchar(2048) NOT NULL)`
+                              encrypted_key varchar(2048) NOT NULL,
+                              CONSTRAINT public_key_id_fk
+                                FOREIGN KEY (public_key_id)
+                                REFERENCES public_key (id)
+                                ON DELETE CASCADE
+                                ON UPDATE CASCADE
+                            )`
 
 	sqlCreateNamedSeqTable = `CREATE TABLE IF NOT EXISTS named_seq(
                             id INTEGER NOT NULL PRIMARY KEY,
-                            user_id integer REFERENCES charm_user (id) NOT NULL,
+                            user_id integer NOT NULL,
                             seq integer NOT NULL DEFAULT 0,
                             name varchar(1024) NOT NULL,
-                            UNIQUE (user_id, name))`
+                            UNIQUE (user_id, name),
+                            CONSTRAINT user_id_fk
+                                FOREIGN KEY (user_id)
+                                REFERENCES charm_user (id)
+                                ON DELETE CASCADE
+                                ON UPDATE CASCADE
+                            )`
 
 	sqlCreateNewsTable = `CREATE TABLE IF NOT EXISTS news(
                         id INTEGER NOT NULL PRIMARY KEY,
                         subject text,
                         body text,
-                        created_at timestamp default current_timestamp)`
+                        created_at timestamp default current_timestamp
+                        )`
 
 	sqlCreateNewsTagTable = `CREATE TABLE IF NOT EXISTS news_tag(
                            id INTEGER NOT NULL PRIMARY KEY,
                            tag varchar(250),
-                           news_id integer REFERENCES news (id) NOT NULL)`
+                           news_id integer NOT NULL,
+                           CONSTRAINT news_id_fk
+                                FOREIGN KEY (news_id)
+                                REFERENCES news (id)
+                                ON DELETE CASCADE
+                                ON UPDATE CASCADE
+                           )`
 
 	sqlCreateTokenTable = `CREATE TABLE IF NOT EXISTS token(
                            id INTEGER NOT NULL PRIMARY KEY,
                            pin text UNIQUE NOT NULL,
-                           created_at timestamp default current_timestamp)`
+                           created_at timestamp default current_timestamp
+                           )`
 
 	sqlSelectUserWithName         = `SELECT id, charm_id, name, email, bio, created_at FROM charm_user WHERE name like ?`
 	sqlSelectUserWithCharmID      = `SELECT id, charm_id, name, email, bio, created_at FROM charm_user WHERE charm_id = ?`
