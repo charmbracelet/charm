263be23025a1b2d57abaa10edc54bf132b0d4e6b
Toby Padilla <toby@charm.sh>
2022-02-18 11:48:26 -0600

    Improve migrate output and add verbose flag

--- cmd/migrate_account.go
+++ cmd/migrate_account.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/charmbracelet/charm/client"
@@ -10,91 +11,142 @@ import (
 
 // MigrateAccountCmd is a command to convert your legacy RSA SSH keys to the
 // new Ed25519 standard keys
-var MigrateAccountCmd = &cobra.Command{
-	Use:    "migrate-account",
-	Hidden: true,
-	Short:  "",
-	Long:   "",
-	Args:   cobra.NoArgs,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		rcfg, err := client.ConfigFromEnv()
-		if err != nil {
-			return err
-		}
-		rcfg.KeyType = "rsa"
-		rsaClient, err := client.NewClient(rcfg)
-		if err != nil {
-			return err
-		}
+var (
+	verbose   bool
+	linkError bool
 
-		ecfg, err := client.ConfigFromEnv()
-		if err != nil {
-			return err
-		}
-		ecfg.KeyType = "ed25519"
-		ed25519Client, err := client.NewClient(ecfg)
-		if err != nil {
-			return err
-		}
-		lh := &linkHandler{linkChan: make(chan string)}
-		go func() {
-			rsaClient.LinkGen(lh)
-		}()
-		tok := <-lh.linkChan
-		ed25519Client.Link(lh, tok)
-		err = rsaClient.SyncEncryptKeys()
-		if err != nil {
-			return err
-		}
-		err = ed25519Client.SyncEncryptKeys()
-		if err != nil {
-		}
-		return nil
-	},
-}
+	MigrateAccountCmd = &cobra.Command{
+		Use:    "migrate-account",
+		Hidden: true,
+		Short:  "",
+		Long:   "",
+		Args:   cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Println("Migrating account...")
+			rcfg, err := client.ConfigFromEnv()
+			if err != nil {
+				return err
+			}
+			rcfg.KeyType = "rsa"
+			rsaClient, err := client.NewClient(rcfg)
+			if err != nil {
+				return err
+			}
+
+			ecfg, err := client.ConfigFromEnv()
+			if err != nil {
+				return err
+			}
+			ecfg.KeyType = "ed25519"
+			ed25519Client, err := client.NewClient(ecfg)
+			if err != nil {
+				return err
+			}
+
+			lc := make(chan string)
+			go func() {
+				lh := &linkHandler{desc: "link-gen", linkChan: lc}
+				rsaClient.LinkGen(lh)
+			}()
+			tok := <-lc
+			lh := &linkHandler{desc: "link-request", linkChan: lc}
+			ed25519Client.Link(lh, tok)
+			if verbose {
+				log.Printf("link-gen sync encrypt keys")
+			}
+			err = rsaClient.SyncEncryptKeys()
+			if err != nil {
+				if verbose {
+					log.Printf("link-gen sync encrypt keys failed")
+				} else {
+					printError()
+				}
+				return err
+			}
+			if verbose {
+				log.Printf("link-request sync encrypt keys")
+			}
+			err = ed25519Client.SyncEncryptKeys()
+			if err != nil {
+				if verbose {
+					log.Printf("link-request sync encrypt keys failed")
+				} else {
+					printError()
+				}
+				return err
+			}
+			if !linkError {
+				fmt.Println("Account migrated! You're good to go.")
+			} else {
+				printError()
+			}
+			return nil
+		},
+	}
+)
 
 type linkHandler struct {
+	desc     string
 	linkChan chan string
 }
 
 func (lh *linkHandler) TokenCreated(l *proto.Link) {
-	// log.Printf("token created %v", l)
+	lh.printDebug("token created", l)
 	lh.linkChan <- string(l.Token)
+	lh.printDebug("token created sent to chan", l)
 }
 
 func (lh *linkHandler) TokenSent(l *proto.Link) {
-	// log.Printf("token sent %v", l)
+	lh.printDebug("token sent", l)
 }
 
 func (lh *linkHandler) ValidToken(l *proto.Link) {
-	// log.Printf("valid token %v", l)
+	lh.printDebug("valid token", l)
 }
 
 func (lh *linkHandler) InvalidToken(l *proto.Link) {
-	// log.Printf("invalid token %v", l)
+	lh.printDebug("invalid token", l)
 }
 
 func (lh *linkHandler) Request(l *proto.Link) bool {
-	// log.Printf("request %v", l)
+	lh.printDebug("request", l)
 	return true
 }
 
 func (lh *linkHandler) RequestDenied(l *proto.Link) {
-	// log.Printf("request denied %v", l)
+	lh.printDebug("request denied", l)
 }
 
 func (lh *linkHandler) SameUser(l *proto.Link) {
-	log.Println("Success! Looks like you've done this already.")
+	lh.printDebug("same user", l)
 }
 
 func (lh *linkHandler) Success(l *proto.Link) {
-	log.Println("Success! You're good to go.")
+	lh.printDebug("success", l)
 }
 
 func (lh *linkHandler) Timeout(l *proto.Link) {
-	log.Println("Timeout")
+	lh.printDebug("timeout", l)
 }
 
 func (lh linkHandler) Error(l *proto.Link) {
-	log.Printf("Error %v", l)
+	linkError = true
+	lh.printDebug("error", l)
+	if !verbose {
+		printError()
+	}
+}
+
+func (lh *linkHandler) printDebug(msg string, l *proto.Link) {
+	if verbose {
+		log.Printf("%s %s:\t%v\n", lh.desc, msg, l)
+	}
+}
+
+func printError() {
+	fmt.Println("\nThere was an error migrating your account. Please re-run with the -v argument `charm migrate-account -v` and join our slack at https://charm.sh/slack to help debug the issue. Sorry about that, we'll try to figure it out!")
+}
+
+func init() {
+	MigrateAccountCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "print debug output")
 }
