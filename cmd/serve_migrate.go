charmbracelet/charm

commit 43c8a102702e504e306a7a43f9ed8d522bef32b8

feat: add db migrate cmd and first sql migration

cmd/serve_migrate.go
@@ -1,0 +1,61 @@
+package cmd
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/charmbracelet/charm/server"
+	"github.com/charmbracelet/charm/server/db/sqlite"
+	"github.com/charmbracelet/charm/server/db/sqlite/migration"
+	"github.com/spf13/cobra"
+	_ "modernc.org/sqlite"
+)
+
+var (
+	ServeMigrationCmd = &cobra.Command{
+		Use:     "migrate",
+		Aliases: []string{"migration"},
+		Hidden:  true,
+		Short:   "Run the server migration tool.",
+		Long:    paragraph("Run the server migration tool to migrate the database."),
+		Args:    cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cfg := server.DefaultConfig()
+			dp := filepath.Join(cfg.DataDir, "db", sqlite.DbName)
+			_, err := os.Stat(dp)
+			if err != nil {
+				return fmt.Errorf("database does not exist: %s", err)
+			}
+			log.Printf("Opening SQLite db: %s\n", dp)
+			db, err := sql.Open("sqlite", dp+sqlite.DbOptions)
+			if err != nil {
+				return err
+			}
+			for _, m := range []migration.Migration{
+				migration.Migration0001,
+			} {
+				log.Printf("Running migration: %04d %s\n", m.ID, m.Name)
+				tx, err := db.Begin()
+				if err != nil {
+					return err
+				}
+				_, err = tx.Exec(m.Sql)
+				if err != nil {
+					return err
+				}
+				err = tx.Commit()
+				if err != nil {
+					return err
+				}
+				return nil
+			}
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+)

server/db/sqlite/migration/0001_foreign_keys.go
@@ -1,0 +1,95 @@
+package migration
+
+var (
+	Migration0001 = Migration{
+		ID:   1,
+		Name: "foreign keys",
+		Sql: `
+PRAGMA foreign_keys=off;
+
+/* public_key */
+ALTER TABLE public_key RENAME TO _public_key;
+
+CREATE TABLE public_key(
+	id INTEGER NOT NULL PRIMARY KEY,
+	user_id integer NOT NULL,
+	public_key varchar(2048) NOT NULL,
+	created_at timestamp default current_timestamp,
+	UNIQUE (user_id, public_key),
+	CONSTRAINT user_id_fk
+	FOREIGN KEY (user_id)
+		REFERENCES charm_user (id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
+
+INSERT INTO public_key SELECT * FROM _public_key;
+
+DROP TABLE _public_key;
+/* public_key */
+
+/* encrypt_key */
+ALTER TABLE encrypt_key RENAME TO _encrypt_key;
+
+CREATE TABLE encrypt_key(
+	id INTEGER NOT NULL PRIMARY KEY,
+	public_key_id integer NOT NULL,
+	global_id uuid NOT NULL,
+	created_at timestamp default current_timestamp,
+	encrypted_key varchar(2048) NOT NULL,
+	CONSTRAINT public_key_id_fk
+		FOREIGN KEY (public_key_id)
+		REFERENCES public_key (id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
+
+INSERT INTO encrypt_key SELECT * FROM _encrypt_key;
+
+DROP TABLE _encrypt_key;
+/* encrypt_key */
+
+/* named_seq */
+ALTER TABLE named_seq RENAME TO _named_seq;
+
+CREATE TABLE named_seq(
+	id INTEGER NOT NULL PRIMARY KEY,
+	user_id integer NOT NULL,
+	seq integer NOT NULL DEFAULT 0,
+	name varchar(1024) NOT NULL,
+	UNIQUE (user_id, name),
+	CONSTRAINT user_id_fk
+		FOREIGN KEY (user_id)
+		REFERENCES charm_user (id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
+
+INSERT INTO named_seq SELECT * FROM _named_seq;
+
+DROP TABLE _named_seq;
+/* named_seq */
+
+/* news_tag */
+ALTER TABLE news_tag RENAME TO _news_tag;
+
+CREATE TABLE news_tag(
+	id INTEGER NOT NULL PRIMARY KEY,
+	tag varchar(250),
+	news_id integer NOT NULL,
+	CONSTRAINT news_id_fk
+		FOREIGN KEY (news_id)
+		REFERENCES news (id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
+
+INSERT INTO news_tag SELECT * FROM _news_tag;
+
+DROP TABLE _news_tag;
+/* news_tag */
+
+PRAGMA foreign_keys=on;
+`,
+	}
+)

cmd/serve.go
@@ -69,6 +69,9 @@ var (
 )
 
 func init() {
+	ServeCmd.AddCommand(
+		ServeMigrationCmd,
+	)
 	ServeCmd.Flags().IntVar(&serverHTTPPort, "http-port", 0, "HTTP port to listen on")
 	ServeCmd.Flags().IntVar(&serverSSHPort, "ssh-port", 0, "SSH port to listen on")
 	ServeCmd.Flags().IntVar(&serverHealthPort, "health-port", 0, "Health port to listen on")

server/db/sqlite/migration/migration.go
@@ -1,0 +1,7 @@
+package migration
+
+type Migration struct {
+	ID   int
+	Name string
+	Sql  string
+}

commit 39fec6a4898ecc7884fd1f8b0c04a5d90dd51b96

fix: serve migrate
* Wrap migrations in transactions
* Fix migration 0001 where the data gets dropped before they're inserted
  into the new tables
* Bump modernc.org/sqlite

Fixes: 43c8a102702e ("feat: add db migrate cmd and first sql migration")

cmd/serve_migrate.go
@@ -29,28 +29,21 @@ var (
 			if err != nil {
 				return fmt.Errorf("database does not exist: %s", err)
 			}
-			log.Printf("Opening SQLite db: %s\n", dp)
-			db, err := sql.Open("sqlite", dp+sqlite.DbOptions)
-			if err != nil {
-				return err
-			}
+			db := sqlite.NewDB(dp)
 			for _, m := range []migration.Migration{
 				migration.Migration0001,
 			} {
 				log.Printf("Running migration: %04d %s\n", m.ID, m.Name)
-				tx, err := db.Begin()
+				err = db.WrapTransaction(func(tx *sql.Tx) error {
+					_, err := tx.Exec(m.Sql)
+					if err != nil {
+						return err
+					}
+					return nil
+				})
 				if err != nil {
-					return err
+					break
 				}
-				_, err = tx.Exec(m.Sql)
-				if err != nil {
-					return err
-				}
-				err = tx.Commit()
-				if err != nil {
-					return err
-				}
-				return nil
 			}
 			if err != nil {
 				return err

go.mod
@@ -27,7 +27,7 @@ require (
 	goji.io v2.0.2+incompatible
 	golang.org/x/crypto v0.0.0-20211202192323-5770296d904e
 	gopkg.in/square/go-jose.v2 v2.6.0
-	modernc.org/sqlite v1.14.2
+	modernc.org/sqlite v1.14.8
 )
 
 require (
@@ -67,9 +67,9 @@ require (
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 	lukechampine.com/uint128 v1.1.1 // indirect
-	modernc.org/cc/v3 v3.35.18 // indirect
-	modernc.org/ccgo/v3 v3.12.82 // indirect
-	modernc.org/libc v1.11.87 // indirect
+	modernc.org/cc/v3 v3.35.22 // indirect
+	modernc.org/ccgo/v3 v3.15.14 // indirect
+	modernc.org/libc v1.14.6 // indirect
 	modernc.org/mathutil v1.4.1 // indirect
 	modernc.org/memory v1.0.5 // indirect
 	modernc.org/opt v0.1.1 // indirect

go.sum
@@ -175,8 +175,8 @@ github.com/mattn/go-runewidth v0.0.10/go.mod h1:RAqKPSqVFrSLVXbA8x7dzmKdmGzieGRC
 github.com/mattn/go-runewidth v0.0.12/go.mod h1:RAqKPSqVFrSLVXbA8x7dzmKdmGzieGRCM46jaSJTDAk=
 github.com/mattn/go-runewidth v0.0.13 h1:lTGmDsbAYt5DmK6OnoV7EuIF1wEIFAcxld6ypU4OSgU=
 github.com/mattn/go-runewidth v0.0.13/go.mod h1:Jdepj2loyihRzMpdS35Xk/zdY8IAYHsh153qUoGf23w=
-github.com/mattn/go-sqlite3 v1.14.9 h1:10HX2Td0ocZpYEjhilsuo6WWtUqttj2Kb0KtD86/KYA=
-github.com/mattn/go-sqlite3 v1.14.9/go.mod h1:NyWgC/yNuGj7Q9rpYnZvas74GogHl5/Z4A/KQRfk6bU=
+github.com/mattn/go-sqlite3 v1.14.10 h1:MLn+5bFRlWMGoSRmJour3CL1w/qL96mvipqpwQW/Sfk=
+github.com/mattn/go-sqlite3 v1.14.10/go.mod h1:NyWgC/yNuGj7Q9rpYnZvas74GogHl5/Z4A/KQRfk6bU=
 github.com/matttproud/golang_protobuf_extensions v1.0.1/go.mod h1:D8He9yQNgCq6Z5Ld7szi9bcBfOoFv/3dc6xSMkL2PC0=
 github.com/meowgorithm/babylogger v1.2.0 h1:lV48OR+bMVR4qVIaUrcTcITGhfV+C/gKnTYCUR++APU=
 github.com/meowgorithm/babylogger v1.2.0/go.mod h1:Kmw1fbhkP4sLJmhiGIpThiG+guQAQ8dQ3GnLa+8Fjf0=
@@ -427,8 +427,10 @@ modernc.org/cc/v3 v3.35.10/go.mod h1:iPJg1pkwXqAV16SNgFBVYmggfMg6xhs+2oiO0vclK3g
 modernc.org/cc/v3 v3.35.15/go.mod h1:iPJg1pkwXqAV16SNgFBVYmggfMg6xhs+2oiO0vclK3g=
 modernc.org/cc/v3 v3.35.16/go.mod h1:iPJg1pkwXqAV16SNgFBVYmggfMg6xhs+2oiO0vclK3g=
 modernc.org/cc/v3 v3.35.17/go.mod h1:iPJg1pkwXqAV16SNgFBVYmggfMg6xhs+2oiO0vclK3g=
-modernc.org/cc/v3 v3.35.18 h1:rMZhRcWrba0y3nVmdiQ7kxAgOOSq2m2f2VzjHLgEs6U=
 modernc.org/cc/v3 v3.35.18/go.mod h1:iPJg1pkwXqAV16SNgFBVYmggfMg6xhs+2oiO0vclK3g=
+modernc.org/cc/v3 v3.35.20/go.mod h1:iPJg1pkwXqAV16SNgFBVYmggfMg6xhs+2oiO0vclK3g=
+modernc.org/cc/v3 v3.35.22 h1:BzShpwCAP7TWzFppM4k2t03RhXhgYqaibROWkrWq7lE=
+modernc.org/cc/v3 v3.35.22/go.mod h1:iPJg1pkwXqAV16SNgFBVYmggfMg6xhs+2oiO0vclK3g=
 modernc.org/ccgo/v3 v3.9.5/go.mod h1:umuo2EP2oDSBnD3ckjaVUXMrmeAw8C8OSICVa0iFf60=
 modernc.org/ccgo/v3 v3.10.0/go.mod h1:c0yBmkRFi7uW4J7fwx/JiijwOjeAeR2NoSaRVFPmjMw=
 modernc.org/ccgo/v3 v3.11.0/go.mod h1:dGNposbDp9TOZ/1KBxghxtUp/bzErD0/0QW4hhSaBMI=
@@ -458,15 +460,24 @@ modernc.org/ccgo/v3 v3.12.55/go.mod h1:rsXiIyJi9psOwiBkplOaHye5L4MOOaCjHg1Fxkj7I
 modernc.org/ccgo/v3 v3.12.56/go.mod h1:ljeFks3faDseCkr60JMpeDb2GSO3TKAmrzm7q9YOcMU=
 modernc.org/ccgo/v3 v3.12.57/go.mod h1:hNSF4DNVgBl8wYHpMvPqQWDQx8luqxDnNGCMM4NFNMc=
 modernc.org/ccgo/v3 v3.12.60/go.mod h1:k/Nn0zdO1xHVWjPYVshDeWKqbRWIfif5dtsIOCUVMqM=
-modernc.org/ccgo/v3 v3.12.65/go.mod h1:D6hQtKxPNZiY6wDBtehSGKFKmyXn53F8nGTpH+POmS4=
 modernc.org/ccgo/v3 v3.12.66/go.mod h1:jUuxlCFZTUZLMV08s7B1ekHX5+LIAurKTTaugUr/EhQ=
 modernc.org/ccgo/v3 v3.12.67/go.mod h1:Bll3KwKvGROizP2Xj17GEGOTrlvB1XcVaBrC90ORO84=
 modernc.org/ccgo/v3 v3.12.73/go.mod h1:hngkB+nUUqzOf3iqsM48Gf1FZhY599qzVg1iX+BT3cQ=
 modernc.org/ccgo/v3 v3.12.81/go.mod h1:p2A1duHoBBg1mFtYvnhAnQyI6vL0uw5PGYLSIgF6rYY=
-modernc.org/ccgo/v3 v3.12.82 h1:wudcnJyjLj1aQQCXF3IM9Gz2X6UNjw+afIghzdtn0v8=
-modernc.org/ccgo/v3 v3.12.82/go.mod h1:ApbflUfa5BKadjHynCficldU1ghjen84tuM5jRynB7w=
-modernc.org/ccorpus v1.11.1 h1:K0qPfpVG1MJh5BYazccnmhywH4zHuOgJXgbjzyp6dWA=
+modernc.org/ccgo/v3 v3.12.84/go.mod h1:ApbflUfa5BKadjHynCficldU1ghjen84tuM5jRynB7w=
+modernc.org/ccgo/v3 v3.12.86/go.mod h1:dN7S26DLTgVSni1PVA3KxxHTcykyDurf3OgUzNqTSrU=
+modernc.org/ccgo/v3 v3.12.90/go.mod h1:obhSc3CdivCRpYZmrvO88TXlW0NvoSVvdh/ccRjJYko=
+modernc.org/ccgo/v3 v3.12.92/go.mod h1:5yDdN7ti9KWPi5bRVWPl8UNhpEAtCjuEE7ayQnzzqHA=
+modernc.org/ccgo/v3 v3.13.1/go.mod h1:aBYVOUfIlcSnrsRVU8VRS35y2DIfpgkmVkYZ0tpIXi4=
+modernc.org/ccgo/v3 v3.15.1/go.mod h1:md59wBwDT2LznX/OTCPoVS6KIsdRgY8xqQwBV+hkTH0=
+modernc.org/ccgo/v3 v3.15.9/go.mod h1:md59wBwDT2LznX/OTCPoVS6KIsdRgY8xqQwBV+hkTH0=
+modernc.org/ccgo/v3 v3.15.10/go.mod h1:wQKxoFn0ynxMuCLfFD09c8XPUCc8obfchoVR9Cn0fI8=
+modernc.org/ccgo/v3 v3.15.12/go.mod h1:VFePOWoCd8uDGRJpq/zfJ29D0EVzMSyID8LCMWYbX6I=
+modernc.org/ccgo/v3 v3.15.14 h1:/Pcjoc5mPznDMH3CErDeX4mHLAAQyR5lzr3s2FpqDY0=
+modernc.org/ccgo/v3 v3.15.14/go.mod h1:144Sz2iBCKogb9OKwsu7hQEub3EVgOlyI8wMUPGKUXQ=
 modernc.org/ccorpus v1.11.1/go.mod h1:2gEUTrWqdpH2pXsmTM1ZkjeSrUWDpjMu2T6m29L/ErQ=
+modernc.org/ccorpus v1.11.6 h1:J16RXiiqiCgua6+ZvQot4yUuUy8zxgqbqEEUuGPlISk=
+modernc.org/ccorpus v1.11.6/go.mod h1:2gEUTrWqdpH2pXsmTM1ZkjeSrUWDpjMu2T6m29L/ErQ=
 modernc.org/httpfs v1.0.6 h1:AAgIpFZRXuYnkjftxTAZwMIiwEqAfk8aVB2/oA6nAeM=
 modernc.org/httpfs v1.0.6/go.mod h1:7dosgurJGp0sPaRanU53W4xZYKh14wfzX420oZADeHM=
 modernc.org/libc v1.9.8/go.mod h1:U1eq8YWr/Kc1RWCMFUWEdkTg8OTcfLw2kY8EDwl039w=
@@ -498,13 +509,20 @@ modernc.org/libc v1.11.54/go.mod h1:S/FVnskbzVUrjfBqlGFIPA5m7UwB3n9fojHhCNfSsnw=
 modernc.org/libc v1.11.55/go.mod h1:j2A5YBRm6HjNkoSs/fzZrSxCuwWqcMYTDPLNx0URn3M=
 modernc.org/libc v1.11.56/go.mod h1:pakHkg5JdMLt2OgRadpPOTnyRXm/uzu+Yyg/LSLdi18=
 modernc.org/libc v1.11.58/go.mod h1:ns94Rxv0OWyoQrDqMFfWwka2BcaF6/61CqJRK9LP7S8=
-modernc.org/libc v1.11.70/go.mod h1:DUOmMYe+IvKi9n6Mycyx3DbjfzSKrdr/0Vgt3j7P5gw=
 modernc.org/libc v1.11.71/go.mod h1:DUOmMYe+IvKi9n6Mycyx3DbjfzSKrdr/0Vgt3j7P5gw=
 modernc.org/libc v1.11.75/go.mod h1:dGRVugT6edz361wmD9gk6ax1AbDSe0x5vji0dGJiPT0=
 modernc.org/libc v1.11.82/go.mod h1:NF+Ek1BOl2jeC7lw3a7Jj5PWyHPwWD4aq3wVKxqV1fI=
 modernc.org/libc v1.11.86/go.mod h1:ePuYgoQLmvxdNT06RpGnaDKJmDNEkV7ZPKI2jnsvZoE=
-modernc.org/libc v1.11.87 h1:PzIzOqtlzMDDcCzJ5cUP6h/Ku6Fa9iyflP2ccTY64aE=
 modernc.org/libc v1.11.87/go.mod h1:Qvd5iXTeLhI5PS0XSyqMY99282y+3euapQFxM7jYnpY=
+modernc.org/libc v1.11.88/go.mod h1:h3oIVe8dxmTcchcFuCcJ4nAWaoiwzKCdv82MM0oiIdQ=
+modernc.org/libc v1.11.98/go.mod h1:ynK5sbjsU77AP+nn61+k+wxUGRx9rOFcIqWYYMaDZ4c=
+modernc.org/libc v1.11.101/go.mod h1:wLLYgEiY2D17NbBOEp+mIJJJBGSiy7fLL4ZrGGZ+8jI=
+modernc.org/libc v1.12.0/go.mod h1:2MH3DaF/gCU8i/UBiVE1VFRos4o523M7zipmwH8SIgQ=
+modernc.org/libc v1.14.1/go.mod h1:npFeGWjmZTjFeWALQLrvklVmAxv4m80jnG3+xI8FdJk=
+modernc.org/libc v1.14.2/go.mod h1:MX1GBLnRLNdvmK9azU9LCxZ5lMyhrbEMK8rG3X/Fe34=
+modernc.org/libc v1.14.3/go.mod h1:GPIvQVOVPizzlqyRX3l756/3ppsAgg1QgPxjr5Q4agQ=
+modernc.org/libc v1.14.6 h1:SSiZiE5199iYsGM9gtkDj90xqcXVwubWG8CtoYE+Mnk=
+modernc.org/libc v1.14.6/go.mod h1:2PJHINagVxO4QW/5OQdRrvMYo+bm5ClpUFfyXCYl9ak=
 modernc.org/mathutil v1.1.1/go.mod h1:mZW8CKdRPY1v87qxC/wUdX5O1qDzXMP5TH3wjfpga6E=
 modernc.org/mathutil v1.2.2/go.mod h1:mZW8CKdRPY1v87qxC/wUdX5O1qDzXMP5TH3wjfpga6E=
 modernc.org/mathutil v1.4.0/go.mod h1:mZW8CKdRPY1v87qxC/wUdX5O1qDzXMP5TH3wjfpga6E=
@@ -515,14 +533,15 @@ modernc.org/memory v1.0.5 h1:XRch8trV7GgvTec2i7jc33YlUI0RKVDBvZ5eZ5m8y14=
 modernc.org/memory v1.0.5/go.mod h1:B7OYswTRnfGg+4tDH1t1OeUNnsy2viGTdME4tzd+IjM=
 modernc.org/opt v0.1.1 h1:/0RX92k9vwVeDXj+Xn23DKp2VJubL7k8qNffND6qn3A=
 modernc.org/opt v0.1.1/go.mod h1:WdSiB5evDcignE70guQKxYUl14mgWtbClRi5wmkkTX0=
-modernc.org/sqlite v1.14.2 h1:ohsW2+e+Qe2To1W6GNezzKGwjXwSax6R+CrhRxVaFbE=
-modernc.org/sqlite v1.14.2/go.mod h1:yqfn85u8wVOE6ub5UT8VI9JjhrwBUUCNyTACN0h6Sx8=
+modernc.org/sqlite v1.14.8 h1:2OOqfZAyU4x4qusilvHoRXXqsAgaZobi1o+mjQ5MUpw=
+modernc.org/sqlite v1.14.8/go.mod h1:TFmXjym+/jR31fxc2B5eHnKMuJJGY7i1L/T5A0jzVww=
 modernc.org/strutil v1.1.1 h1:xv+J1BXY3Opl2ALrBwyfEikFAj8pmqcpnfmuwUwcozs=
 modernc.org/strutil v1.1.1/go.mod h1:DE+MQQ/hjKBZS2zNInV5hhcipt5rLPWkmpbGeW5mmdw=
-modernc.org/tcl v1.8.13 h1:V0sTNBw0Re86PvXZxuCub3oO9WrSTqALgrwNZNvLFGw=
-modernc.org/tcl v1.8.13/go.mod h1:V+q/Ef0IJaNUSECieLU4o+8IScapxnMyFV6i/7uQlAY=
+modernc.org/tcl v1.11.0 h1:B/zzEYjINeaki38KcIqdQRQx7W3WE7TkrlTwGnbm2II=
+modernc.org/tcl v1.11.0/go.mod h1:zsTUpbQ+NxQEjOjCUlImDLPv1sG8Ww0qp66ZvyOxCgw=
 modernc.org/token v1.0.0 h1:a0jaWiNMDhDUtqOj09wvjWWAqd3q7WpBulmL9H2egsk=
 modernc.org/token v1.0.0/go.mod h1:UGzOrNV1mAFSEB63lOFHIpNRUVMvYTc6yu1SMY/XTDM=
-modernc.org/z v1.2.19 h1:BGyRFWhDVn5LFS5OcX4Yd/MlpRTOc7hOPTdcIpCiUao=
-modernc.org/z v1.2.19/go.mod h1:+ZpP0pc4zz97eukOzW3xagV/lS82IpPN9NGG5pNF9vY=
+modernc.org/z v1.3.0/go.mod h1:+mvgLH814oDjtATDdT3rs84JnUIpkvAF5B8AVkNlE2g=
+modernc.org/z v1.3.1 h1:jd/XnJ5W82v0cEpDQOQPpDJSH7H8olKpMqPFKEcM49E=
+modernc.org/z v1.3.1/go.mod h1:0RBFPpdFNiKpjTza1WYaB4+6ySjS6dLBoo09OQZ4E3w=
 rsc.io/pdf v0.1.1/go.mod h1:n8OzWcQ6Sp37PL01nO98y4iUCRdTGarVfzxY20ICaU4=

server/db/sqlite/migration/0001_foreign_keys.go
@@ -24,8 +24,6 @@ CREATE TABLE public_key(
 );
 
 INSERT INTO public_key SELECT * FROM _public_key;
-
-DROP TABLE _public_key;
 /* public_key */
 
 /* encrypt_key */
@@ -45,8 +43,6 @@ CREATE TABLE encrypt_key(
 );
 
 INSERT INTO encrypt_key SELECT * FROM _encrypt_key;
-
-DROP TABLE _encrypt_key;
 /* encrypt_key */
 
 /* named_seq */
@@ -66,8 +62,6 @@ CREATE TABLE named_seq(
 );
 
 INSERT INTO named_seq SELECT * FROM _named_seq;
-
-DROP TABLE _named_seq;
 /* named_seq */
 
 /* news_tag */
@@ -85,9 +79,12 @@ CREATE TABLE news_tag(
 );
 
 INSERT INTO news_tag SELECT * FROM _news_tag;
-
-DROP TABLE _news_tag;
 /* news_tag */
+
+DROP TABLE _public_key;
+DROP TABLE _encrypt_key;
+DROP TABLE _named_seq;
+DROP TABLE _news_tag;
 
 PRAGMA foreign_keys=on;
 `,

server/db/sqlite/storage.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"path/filepath"
 	"strconv"
 	"time"
 
@@ -20,7 +19,7 @@ const (
 	// The DB default file name.
 	DbName = "charm_sqlite.db"
 	// The DB default connection options.
-	DbOptions = "?_pragma=busy_timeout=5000&_pragma=foreign_keys=ON"
+	DbOptions = "?_pragma=busy_timeout(5000)&_pragma=foreign_keys(1)"
 )
 
 type DB struct {
@@ -31,7 +30,7 @@ type DB struct {
 func NewDB(path string) *DB {
 	var err error
 	log.Printf("Opening SQLite db: %s\n", path)
-	db, err := sql.Open("sqlite", filepath.Join(path, DbName)+DbOptions)
+	db, err := sql.Open("sqlite", path+DbOptions)
 	if err != nil {
 		panic(err)
 	}
@@ -84,7 +83,7 @@ func (me *DB) GetUserWithName(name string) (*charm.User, error) {
 func (me *DB) SetUserName(charmID string, name string) (*charm.User, error) {
 	var u *charm.User
 	log.Printf("Setting name `%s` for user %s\n", name, charmID)
-	err := me.wrapTransaction(func(tx *sql.Tx) error {
+	err := me.WrapTransaction(func(tx *sql.Tx) error {
 		var err error
 		r := me.selectUserWithName(tx, name)
 		u, err = me.scanUser(r)
@@ -126,7 +125,7 @@ func (me *DB) SetUserName(charmID string, name string) (*charm.User, error) {
 func (me *DB) UserForKey(key string, create bool) (*charm.User, error) {
 	pk := &charm.PublicKey{}
 	u := &charm.User{}
-	err := me.wrapTransaction(func(tx *sql.Tx) error {
+	err := me.WrapTransaction(func(tx *sql.Tx) error {
 		var err error
 		r := me.selectPublicKey(tx, key)
 		err = r.Scan(&pk.ID, &pk.UserID, &pk.Key)
@@ -168,7 +167,7 @@ func (me *DB) UserForKey(key string, create bool) (*charm.User, error) {
 
 func (me *DB) AddEncryptKeyForPublicKey(u *charm.User, pk string, gid string, ek string, ca *time.Time) error {
 	log.Printf("Adding encrypted key %s %s for user %s\n", gid, ca, u.CharmID)
-	return me.wrapTransaction(func(tx *sql.Tx) error {
+	return me.WrapTransaction(func(tx *sql.Tx) error {
 		u2, err := me.UserForKey(pk, false)
 		if err != nil {
 			return err
@@ -193,7 +192,7 @@ func (me *DB) AddEncryptKeyForPublicKey(u *charm.User, pk string, gid string, ek
 
 func (me *DB) EncryptKeysForPublicKey(pk *charm.PublicKey) ([]*charm.EncryptKey, error) {
 	var ks []*charm.EncryptKey
-	err := me.wrapTransaction(func(tx *sql.Tx) error {
+	err := me.WrapTransaction(func(tx *sql.Tx) error {
 		rs, err := me.selectEncryptKeys(tx, pk.ID)
 		if err != nil {
 			return err
@@ -217,7 +216,7 @@ func (me *DB) EncryptKeysForPublicKey(pk *charm.PublicKey) ([]*charm.EncryptKey,
 func (me *DB) LinkUserKey(user *charm.User, key string) error {
 	ks := charm.PublicKeySha(key)
 	log.Printf("Linking user %s and key %s\n", user.CharmID, ks)
-	return me.wrapTransaction(func(tx *sql.Tx) error {
+	return me.WrapTransaction(func(tx *sql.Tx) error {
 		return me.insertPublicKey(tx, user.ID, key)
 	})
 }
@@ -225,7 +224,7 @@ func (me *DB) LinkUserKey(user *charm.User, key string) error {
 func (me *DB) UnlinkUserKey(user *charm.User, key string) error {
 	ks := charm.PublicKeySha(key)
 	log.Printf("Unlinking user %s key %s\n", user.CharmID, ks)
-	return me.wrapTransaction(func(tx *sql.Tx) error {
+	return me.WrapTransaction(func(tx *sql.Tx) error {
 		err := me.deleteUserPublicKey(tx, user.ID, key)
 		if err != nil {
 			return err
@@ -252,7 +251,7 @@ func (me *DB) UnlinkUserKey(user *charm.User, key string) error {
 func (me *DB) KeysForUser(user *charm.User) ([]*charm.PublicKey, error) {
 	var keys []*charm.PublicKey
 	log.Printf("Getting keys for user %s\n", user.CharmID)
-	err := me.wrapTransaction(func(tx *sql.Tx) error {
+	err := me.WrapTransaction(func(tx *sql.Tx) error {
 		rs, err := me.selectUserPublicKeys(tx, user.ID)
 		if err != nil {
 			return err
@@ -283,7 +282,7 @@ func (me *DB) KeysForUser(user *charm.User) ([]*charm.PublicKey, error) {
 func (me *DB) GetSeq(u *charm.User, name string) (uint64, error) {
 	var seq uint64
 	var err error
-	err = me.wrapTransaction(func(tx *sql.Tx) error {
+	err = me.WrapTransaction(func(tx *sql.Tx) error {
 		seq, err = me.selectNamedSeq(tx, u.ID, name)
 		if err == sql.ErrNoRows {
 			seq, err = me.incNamedSeq(tx, u.ID, name)
@@ -299,7 +298,7 @@ func (me *DB) GetSeq(u *charm.User, name string) (uint64, error) {
 func (me *DB) NextSeq(u *charm.User, name string) (uint64, error) {
 	var seq uint64
 	var err error
-	err = me.wrapTransaction(func(tx *sql.Tx) error {
+	err = me.WrapTransaction(func(tx *sql.Tx) error {
 		seq, err = me.incNamedSeq(tx, u.ID, name)
 		if err != nil {
 			return err
@@ -318,7 +317,7 @@ func (me *DB) GetNews(id string) (*charm.News, error) {
 	if err != nil {
 		return nil, err
 	}
-	err = me.wrapTransaction(func(tx *sql.Tx) error {
+	err = me.WrapTransaction(func(tx *sql.Tx) error {
 		r := me.selectNews(tx, i)
 		return r.Scan(&n.ID, &n.Subject, &n.Body, &n.CreatedAt)
 	})
@@ -330,7 +329,7 @@ func (me *DB) GetNews(id string) (*charm.News, error) {
 
 func (me *DB) GetNewsList(tag string, page int) ([]*charm.News, error) {
 	var ns []*charm.News
-	err := me.wrapTransaction(func(tx *sql.Tx) error {
+	err := me.WrapTransaction(func(tx *sql.Tx) error {
 		rs, err := me.selectNewsList(tx, tag, page)
 		if err != nil {
 			return err
@@ -349,13 +348,13 @@ func (me *DB) GetNewsList(tag string, page int) ([]*charm.News, error) {
 }
 
 func (me *DB) PostNews(subject string, body string, tags []string) error {
-	return me.wrapTransaction(func(tx *sql.Tx) error {
+	return me.WrapTransaction(func(tx *sql.Tx) error {
 		return me.insertNews(tx, subject, body, tags)
 	})
 }
 
 func (me *DB) MergeUsers(userID1 int, userID2 int) error {
-	return me.wrapTransaction(func(tx *sql.Tx) error {
+	return me.WrapTransaction(func(tx *sql.Tx) error {
 		err := me.updateMergePublicKeys(tx, userID1, userID2)
 		if err != nil {
 			return err
@@ -366,7 +365,7 @@ func (me *DB) MergeUsers(userID1 int, userID2 int) error {
 }
 
 func (me *DB) SetToken(token charm.Token) error {
-	return me.wrapTransaction(func(tx *sql.Tx) error {
+	return me.WrapTransaction(func(tx *sql.Tx) error {
 		err := me.insertToken(tx, string(token))
 		if err != nil {
 			serr, ok := err.(*sqlite.Error)
@@ -379,13 +378,13 @@ func (me *DB) SetToken(token charm.Token) error {
 }
 
 func (me *DB) DeleteToken(token charm.Token) error {
-	return me.wrapTransaction(func(tx *sql.Tx) error {
+	return me.WrapTransaction(func(tx *sql.Tx) error {
 		return me.deleteToken(tx, string(token))
 	})
 }
 
 func (me *DB) CreateDB() error {
-	return me.wrapTransaction(func(tx *sql.Tx) error {
+	return me.WrapTransaction(func(tx *sql.Tx) error {
 		err := me.createUserTable(tx)
 		if err != nil {
 			return err
@@ -626,7 +625,7 @@ func (me *DB) execOrPanic(tx *sql.Tx, s string) {
 	}
 }
 
-func (me *DB) wrapTransaction(f func(tx *sql.Tx) error) error {
+func (me *DB) WrapTransaction(f func(tx *sql.Tx) error) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer func() { cancel() }()
 	tx, err := me.db.BeginTx(ctx, nil)

server/server.go
@@ -167,7 +167,7 @@ func (srv *Server) init(cfg *Config) {
 		if err != nil {
 			log.Fatalf("could not init sqlite path: %s", err)
 		}
-		db := sqlite.NewDB(dp)
+		db := sqlite.NewDB(filepath.Join(dp, sqlite.DbName))
 		srv.Config = cfg.WithDB(db)
 	}
 	if cfg.FileStore == nil {