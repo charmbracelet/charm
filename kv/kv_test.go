d58a23e0261f726af949754e81d6eebcc83ec54c
bashbunni <bashbunni.io@gmail.com>
2022-03-08 11:20:02 -0800

    test: add Sync, OptionsWithEncryption

--- kv/kv_test.go
+++ kv/kv_test.go
@@ -128,7 +128,78 @@ func TestDelete(t *testing.T) {
 				t.Errorf("%s: expected an empty string %s, got %s", tc.testname, want, get)
 
 			}
-
 		}
+	}
+}
+
+// TestSync
+
+// TODO: get more test cases
+func TestSync(t *testing.T) {
+	kv := setup(t)
+	err := kv.Sync()
+	if err != nil {
+		t.Errorf("unexpected error")
+	}
+}
+
+// TestOptionsWithEncryption
+
+func TestOptionsWithEncryption(t *testing.T) {
+	_, err := OptionsWithEncryption(badger.DefaultOptions(""), []byte("1234"), -2)
+	if err == nil {
+		t.Errorf("expected an error")
+	}
+}
+
+// TestKeys
+
+func TestKeys(t *testing.T) {
+	tests := []struct {
+		testname string
+		want     [][]byte
+	}{
+		{"single key with empty value", [][]byte{[]byte("1234")}},
+		{"multiple valid keys", [][]byte{[]byte("1234"), []byte("5678")}},
+		{"some invalid and valid keys", [][]byte{{}, []byte("1234")}},
+	}
+	for _, tc := range tests {
+		kv := setup(t)
+		kv.addKeys(tc.want)
+		got, err := kv.Keys()
+		if err != nil {
+			t.Errorf("unexpected error")
+		}
+		if !compareKeyLists(got, tc.want) {
+			t.Errorf("got did not match want")
+		}
+	}
+}
+
+func (kv *KV) addKeys(values [][]byte) {
+	for _, val := range values {
+		kv.Set(val, []byte("hello"))
+	}
+}
+
+func compareKeyLists(a, b [][]byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if bytes.Compare(a[i], b[i]) != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// TestReset
+
+func TestReset(t *testing.T) {
+	kv := setup(t)
+	err := kv.Reset()
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
 	}
 }
