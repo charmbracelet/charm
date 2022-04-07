4c88ad21a2f9bbb31e1512969e57b8bef82ca4f0
bashbunni <bashbunni.io@gmail.com>
2022-04-07 14:10:15 -0700

    test: use TestMain

--- kv/kv_test.go
+++ kv/kv_test.go
@@ -19,52 +19,56 @@ import (
 )
 
 var (
-	cfg *server.Config
-	td  string
+	clientTD string
+	cfg      *server.Config
 )
 
 // Helpers
 
-func startServer(t *testing.T, testName string, testFunc func(*KV, string)) {
-	cfg, td = getTestServerConfig(t)
+func TestMain(m *testing.M) {
+	var clientTD string
+	var s *server.Server
+	cfg = getTestServerConfig()
 	s, err := server.NewServer(cfg)
 	if err != nil {
-		t.Fatalf("new server error: %s", err)
+		log.Fatalf("new server error: %s", err)
 	}
 	go s.Start()
-	kv, pn := setupKV(t)
-	t.Run("health-ping", func(t *testing.T) {
-		_, err := fetchURL(fmt.Sprintf("http://localhost:%d", cfg.HealthPort), 3)
-		if err != nil {
-			t.Fatalf("could not ping server: %s", err)
-		}
-	})
-	t.Run(testName, func(t *testing.T) {
-		testFunc(kv, pn)
-	})
-	t.Cleanup(func() {
-		os.RemoveAll(pn)
-		err := s.Close()
-		if err != nil {
-			log.Printf("error closing server: %s", err)
-		}
-	})
+	_, clientTD = setupKV(setupTestClient(cfg))
+	code := m.Run()
+	os.RemoveAll(clientTD)
+	if err = s.Close(); err != nil {
+		log.Printf("error closing server: %s", err)
+	}
+	os.Exit(code)
 }
 
-func getTestServerConfig(t *testing.T) (*server.Config, string) {
-	// set up template server configurations
+// healthPing check that the server has started
+func healthPing(cfg *server.Config) {
+	_, err := fetchURL(fmt.Sprintf("http://localhost:%d", cfg.HealthPort), 3)
+	if err != nil {
+		log.Fatalf("could not ping server: %s", err)
+	}
+}
+
+// getTestServerConfig create the test server
+func getTestServerConfig() *server.Config {
 	cfg := server.DefaultConfig()
-	td := t.TempDir()
+	td, err := os.MkdirTemp("", "")
+	if err != nil {
+		log.Fatalf("unable to init temp dir: %s", err)
+	}
 	cfg.DataDir = filepath.Join(td, ".data")
 	sp := filepath.Join(td, ".ssh")
 	kp, err := keygen.NewWithWrite(sp, "charm_server", []byte(""), keygen.Ed25519)
 	if err != nil {
-		t.Fatalf("keygen error: %s", err)
+		log.Fatalf("keygen error: %s", err)
 	}
 	cfg = cfg.WithKeys(kp.PublicKey, kp.PrivateKeyPEM)
-	return cfg, td
+	return cfg
 }
 
+// fetchURL check if we can successfully do a GET request to test server
 func fetchURL(url string, retries int) (*http.Response, error) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -80,30 +84,29 @@ func fetchURL(url string, retries int) (*http.Response, error) {
 	return resp, nil
 }
 
-func setupKV(t *testing.T) (*KV, string) {
-	t.Helper()
-	pn, err := ioutil.TempDir("", "charmkv")
+func setupKV(cc *client.Client) (*KV, string) {
+	var err error
+	clientTD, err = ioutil.TempDir("", "charmkv")
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
-	opt := badger.DefaultOptions(pn).WithLoggingLevel(badger.ERROR)
-	cc := setupTestClient(t)
+	opt := badger.DefaultOptions(clientTD).WithLoggingLevel(badger.ERROR)
 	kv, err := Open(cc, "test", opt)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return kv, pn
+	return kv, clientTD
 }
 
-func setupTestClient(t *testing.T) *client.Client {
+func setupTestClient(cfg *server.Config) *client.Client {
 	ccfg, err := client.ConfigFromEnv()
 	if err != nil {
-		t.Fatalf("client config from env error: %s", err)
+		log.Fatalf("client config from env error: %s", err)
 	}
 	ccfg.Host = cfg.Host
 	ccfg.SSHPort = cfg.SSHPort
 	ccfg.HTTPPort = cfg.HTTPPort
-	ccfg.DataDir = filepath.Join(td, ".client-data")
+	ccfg.DataDir = filepath.Join(clientTD, ".client-data")
 	cl, err := client.NewClient(ccfg)
 	if err != nil {
 		log.Printf("unable to create new client: %s", err)
@@ -129,170 +132,165 @@ func TestOpenWithDefaults(t *testing.T) {
 // TestGet
 
 func TestGetForEmptyDB(t *testing.T) {
-	startServer(t, "get for empty DB", func(*KV, string) {
-		kv, pn := setupKV(t)
-		defer os.RemoveAll(pn)
-		_, err := kv.Get([]byte("1234"))
-		if err == nil {
-			t.Errorf("expected error")
-		}
-	})
+	kv, _ := setupKV(setupTestClient(cfg))
+	_, err := kv.Get([]byte("1234"))
+	if err == nil {
+		t.Errorf("expected error")
+	}
 }
 
 func TestGet(t *testing.T) {
-	startServer(t, "get for non-empty DB", func(*KV, string) {
-		tests := []struct {
-			testname  string
-			key       []byte
-			want      []byte
-			expectErr bool
-		}{
-			{"valid kv pair", []byte("1234"), []byte("valid"), false},
-			{"invalid key", []byte{}, []byte{}, true},
-		}
+	tests := []struct {
+		testname  string
+		key       []byte
+		want      []byte
+		expectErr bool
+	}{
+		{"valid kv pair", []byte("1234"), []byte("valid"), false},
+		{"invalid key", []byte{}, []byte{}, true},
+	}
 
-		for _, tc := range tests {
-			kv, pn := setupKV(t)
-			defer os.RemoveAll(pn)
-			kv.Set(tc.key, tc.want)
-			got, err := kv.Get(tc.key)
-			if tc.expectErr {
-				if err == nil {
-					t.Errorf("%s: expected error", tc.testname)
-				}
-			} else {
-				if err != nil {
-					t.Errorf("%s: unexpected error %v", tc.testname, err)
-				}
-				if bytes.Compare(got, tc.want) != 0 {
-					t.Errorf("%s: got %s, want %s", tc.testname, got, tc.want)
-				}
+	for _, tc := range tests {
+		kv, clientTD := setupKV(setupTestClient(cfg))
+		kv.Set(tc.key, tc.want)
+		got, err := kv.Get(tc.key)
+		if tc.expectErr {
+			if err == nil {
+				t.Errorf("%s: expected error", tc.testname)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("%s: unexpected error %v", tc.testname, err)
+			}
+			if bytes.Compare(got, tc.want) != 0 {
+				t.Errorf("%s: got %s, want %s", tc.testname, got, tc.want)
 			}
 		}
-	})
+		os.RemoveAll(clientTD)
+	}
 }
 
 // TestSetReader
 
 func TestSetReader(t *testing.T) {
-	startServer(t, "set reader", func(*KV, string) {
-		tests := []struct {
-			testname  string
-			key       []byte
-			want      string
-			expectErr bool
-		}{
-			{"set valid value", []byte("am key"), "hello I am a very powerful test *flex*", false},
-			{"set empty key", []byte(""), "", true},
-		}
-		for _, tc := range tests {
-			kv, pn := setupKV(t)
-			defer os.RemoveAll(pn)
-			kv.SetReader(tc.key, strings.NewReader(tc.want))
-			got, err := kv.Get(tc.key)
-			if tc.expectErr {
-				if err == nil {
-					t.Errorf("case: %s expected an error but did not get one", tc.testname)
-				}
-			} else {
-				if err != nil {
-					t.Errorf("case: %s unexpected error %v", tc.testname, err)
-				}
-				if bytes.Compare(got, []byte(tc.want)) != 0 {
-					t.Errorf("case: %s got %s, want %s", tc.testname, got, tc.want)
+	tests := []struct {
+		testname  string
+		key       []byte
+		want      string
+		expectErr bool
+	}{
+		{"set valid value", []byte("am key"), "hello I am a very powerful test *flex*", false},
+		{"set empty key", []byte(""), "", true},
+	}
+	for _, tc := range tests {
+		kv, clientTD := setupKV(setupTestClient(cfg))
+		kv.SetReader(tc.key, strings.NewReader(tc.want))
+		got, err := kv.Get(tc.key)
+		if tc.expectErr {
+			if err == nil {
+				t.Errorf("case: %s expected an error but did not get one", tc.testname)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("case: %s unexpected error %v", tc.testname, err)
+			}
+			if bytes.Compare(got, []byte(tc.want)) != 0 {
+				t.Errorf("case: %s got %s, want %s", tc.testname, got, tc.want)
 
-				}
 			}
 		}
-	})
+		os.RemoveAll(clientTD)
+	}
 }
 
 // TestDelete
 
 func TestDelete(t *testing.T) {
-	startServer(t, "set reader", func(*KV, string) {
-		tests := []struct {
-			testname  string
-			key       []byte
-			value     []byte
-			expectErr bool
-		}{
-			{"valid key", []byte("hello"), []byte("value"), false},
-			{"empty key with value", []byte{}, []byte("value"), true},
-			{"empty key no value", []byte{}, []byte{}, true},
-		}
+	tests := []struct {
+		testname  string
+		key       []byte
+		value     []byte
+		expectErr bool
+	}{
+		{"valid key", []byte("hello"), []byte("value"), false},
+		{"empty key with value", []byte{}, []byte("value"), true},
+		{"empty key no value", []byte{}, []byte{}, true},
+	}
 
-		for _, tc := range tests {
-			kv, pn := setupKV(t)
-			defer os.RemoveAll(pn)
-			kv.Set(tc.key, tc.value)
-			if tc.expectErr {
-				if err := kv.Delete(tc.key); err == nil {
-					t.Errorf("%s: expected error", tc.testname)
-				}
-			} else {
-				if err := kv.Delete(tc.key); err != nil {
-					t.Errorf("%s: unexpected error in Delete %v", tc.testname, err)
-				}
-				want := []byte{} // want an empty result
-				if get, _ := kv.Get(tc.key); bytes.Compare(get, want) != 0 {
-					t.Errorf("%s: expected an empty string %s, got %s", tc.testname, want, get)
-				}
+	for _, tc := range tests {
+		kv, clientTD := setupKV(setupTestClient(cfg))
+		kv.Set(tc.key, tc.value)
+		if tc.expectErr {
+			if err := kv.Delete(tc.key); err == nil {
+				t.Errorf("%s: expected error", tc.testname)
+			}
+		} else {
+			if err := kv.Delete(tc.key); err != nil {
+				t.Errorf("%s: unexpected error in Delete %v", tc.testname, err)
+			}
+			want := []byte{} // want an empty result
+			if get, _ := kv.Get(tc.key); bytes.Compare(get, want) != 0 {
+				t.Errorf("%s: expected an empty string %s, got %s", tc.testname, want, get)
 			}
 		}
-	})
+		os.RemoveAll(clientTD)
+	}
 }
 
 // TestSync
 
 func TestSync(t *testing.T) {
-	startServer(t, "set reader", func(*KV, string) {
-		kv, pn := setupKV(t)
-		defer os.RemoveAll(pn)
-		err := kv.Sync()
-		if err != nil {
-			t.Errorf("unexpected error")
-		}
-	})
+	kv, _ := setupKV(setupTestClient(cfg))
+	err := kv.Sync()
+	if err != nil {
+		t.Errorf("unexpected error")
+	}
 }
 
 // TestOptionsWithEncryption
 
 func TestOptionsWithEncryption(t *testing.T) {
-	startServer(t, "set reader", func(*KV, string) {
-		_, err := OptionsWithEncryption(badger.DefaultOptions(""), []byte("1234"), -2)
-		if err == nil {
-			t.Errorf("expected an error")
-		}
-	})
+	_, err := OptionsWithEncryption(badger.DefaultOptions(""), []byte("1234"), -2)
+	if err == nil {
+		t.Errorf("expected an error")
+	}
 }
 
 // TestKeys
 
 func TestKeys(t *testing.T) {
-	startServer(t, "test keys", func(*KV, string) {
-		tests := []struct {
-			testname string
-			keys     [][]byte
-		}{
-			{"single value", [][]byte{[]byte("one")}},
-			{"two values", [][]byte{[]byte("one"), []byte("two")}},
-			{"multiple values", [][]byte{[]byte("one"), []byte("two"), []byte("three")}},
+	tests := []struct {
+		testname string
+		keys     [][]byte
+	}{
+		{"single value", [][]byte{[]byte("one")}},
+		{"two values", [][]byte{[]byte("one"), []byte("two")}},
+	}
+	for _, tc := range tests {
+		kv, clientTD := setupKV(setupTestClient(cfg))
+		kv.addKeys(tc.keys)
+		got, err := kv.Keys()
+		if err != nil {
+			t.Errorf("unexpected error")
 		}
+		if !compareKeyLists(got, tc.keys) {
+			t.Errorf("got: %s want: %s", showKeys(got), showKeys(tc.keys))
+		}
+		os.RemoveAll(clientTD)
+	}
+}
 
-		for _, tc := range tests {
-			kv, pn := setupKV(t)
-			kv.addKeys(tc.keys)
-			got, err := kv.Keys()
-			if err != nil {
-				t.Errorf("unexpected error")
-			}
-			if !compareKeyLists(got, tc.keys) {
-				t.Errorf("got: %s want: %s", showKeys(got), showKeys(tc.keys))
-			}
-			os.RemoveAll(pn)
-		}
-	})
+func TestMultipleKeys(t *testing.T) {
+	kv, _ := setupKV(setupTestClient(cfg))
+	keys := [][]byte{[]byte("one"), []byte("two"), []byte("three")}
+	kv.addKeys(keys)
+	got, err := kv.Keys()
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+	if !compareKeyLists(got, keys) {
+		t.Errorf("got: %s want: %s", showKeys(got), showKeys(keys))
+	}
 }
 
 func (kv *KV) addKeys(values [][]byte) {
@@ -317,7 +315,7 @@ func showKeys(keys [][]byte) string {
 	msg := ""
 	for _, key := range keys {
 		msg += string(key)
-		msg += "\n"
+		msg += ", "
 	}
 	return msg
 }
