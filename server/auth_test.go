7a65d9d7ae8f401ca1771f773da2a92d026efe07
Carlos A Becker <caarlos0@gmail.com>
2022-04-25 15:45:32 -0300

    test: testserver pkg
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- server/auth_test.go
+++ server/auth_test.go
@@ -1,88 +1,27 @@
-package server
+package server_test
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"path/filepath"
 	"testing"
-	"time"
 
-	"github.com/charmbracelet/charm/client"
-	"github.com/charmbracelet/keygen"
+	"github.com/charmbracelet/charm/testserver"
 )
 
 func TestSSHAuthMiddleware(t *testing.T) {
-	cfg := DefaultConfig()
-	td := t.TempDir()
-	cfg.DataDir = filepath.Join(td, ".data")
-	sp := filepath.Join(td, ".ssh")
-	kp, err := keygen.NewWithWrite(filepath.Join(sp, "charm_server"), []byte(""), keygen.Ed25519)
+	cl := testserver.SetupTestServer(t)
+	auth, err := cl.Auth()
 	if err != nil {
-		t.Fatalf("keygen error: %s", err)
+		t.Fatalf("auth error: %s", err)
 	}
-	cfg = cfg.WithKeys(kp.PublicKey(), kp.PrivateKeyPEM())
-	s, err := NewServer(cfg)
-	if err != nil {
-		t.Fatalf("new server error: %s", err)
+	if auth.JWT == "" {
+		t.Fatal("auth error, missing JWT")
 	}
-
-	go s.Start()
-	t.Run("health-ping", func(t *testing.T) {
-		_, err := fetchURL(fmt.Sprintf("http://localhost:%d", cfg.HealthPort), 3)
-		if err != nil {
-			t.Fatal(fmt.Sprintf("could not ping server: %s", err))
-		}
-	})
-	t.Run("api-auth", func(t *testing.T) {
-		ccfg, err := client.ConfigFromEnv()
-		if err != nil {
-			t.Fatalf("client config from env error: %s", err)
-		}
-		ccfg.Host = cfg.Host
-		ccfg.SSHPort = cfg.SSHPort
-		ccfg.HTTPPort = cfg.HTTPPort
-		ccfg.DataDir = filepath.Join(td, ".client-data")
-		cl, err := client.NewClient(ccfg)
-		if err != nil {
-			t.Fatalf("new client error: %s", err)
-		}
-		auth, err := cl.Auth()
-		if err != nil {
-			t.Fatalf("auth error: %s", err)
-		}
-		if auth.JWT == "" {
-			t.Fatal("auth error, missing JWT")
-		}
-		if auth.ID == "" {
-			t.Fatal("auth error, missing ID")
-		}
-		if auth.PublicKey == "" {
-			t.Fatal("auth error, missing PublicKey")
-		}
-		// if len(auth.EncryptKeys) == 0 {
-		// 	t.Fatal("auth error, missing EncryptKeys")
-		// }
-	})
-	t.Cleanup(func() {
-		defer s.Close() // nolint:errcheck
-		if err != nil {
-			log.Printf("error closing server: %s", err)
-		}
-	})
-}
-
-func fetchURL(url string, retries int) (*http.Response, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		if retries > 0 {
-			time.Sleep(time.Second)
-			return fetchURL(url, retries-1)
-		}
-		return nil, err
+	if auth.ID == "" {
+		t.Fatal("auth error, missing ID")
 	}
-	if resp.StatusCode != 200 {
-		return resp, fmt.Errorf("bad http status code: %d", resp.StatusCode)
+	if auth.PublicKey == "" {
+		t.Fatal("auth error, missing PublicKey")
 	}
-	return resp, nil
+	// if len(auth.EncryptKeys) == 0 {
+	// 	t.Fatal("auth error, missing EncryptKeys")
+	// }
 }
