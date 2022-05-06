79b2087806768a42b3beaf6cdc369dbb93fdc2f3
Ayman Bagabas <ayman.bagabas@gmail.com>
2022-05-06 14:57:15 -0400

    fix: use parallel t.Run()

--- cmd/link_test.go
+++ cmd/link_test.go
@@ -2,81 +2,46 @@ package cmd
 
 import (
 	"log"
-	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/charmbracelet/charm/client"
 	"github.com/charmbracelet/charm/proto"
-	"github.com/charmbracelet/charm/server"
 	"github.com/charmbracelet/charm/testserver"
 )
-
-var (
-	cfg *server.Config
-	td  string
-)
-
-func setup(t *testing.T) {
-	backupFilePath := "./charm-keys-backup.tar"
-	_ = os.RemoveAll(backupFilePath)
-	_ = testserver.SetupTestServer(t)
-
-	// run actual tests here
-}
-
-// create mock client
-func setupTestClient(t *testing.T) *client.Client {
-	ccfg, err := client.ConfigFromEnv()
-	if err != nil {
-		t.Fatalf("client config from env error: %s", err)
-	}
-	ccfg.Host = cfg.Host
-	ccfg.SSHPort = cfg.SSHPort
-	ccfg.HTTPPort = cfg.HTTPPort
-	ccfg.DataDir = filepath.Join(td, ".client-data")
-	cl, err := client.NewClient(ccfg)
-	if err != nil {
-		log.Printf("unable to create new client: %s", err)
-	}
-	return cl
-}
-
-func ConfigFromEnv() {
-	panic("unimplemented")
-}
-
-// create mock linkHandlerTest
 
 // TestLinkGen
 func TestLinkGen(t *testing.T) {
 	_ = testserver.SetupTestServer(t)
 	ecfg, err := client.ConfigFromEnv()
 	if err != nil {
-		t.Errorf("unexpected error: %v", err)
+		t.Fatalf("unexpected error: %v", err)
 	}
 	client1, err := client.NewClient(ecfg)
 	if err != nil {
-		t.Errorf("error creating first client: %v", err)
+		t.Fatalf("error creating first client: %v", err)
 	}
 	client2, err := client.NewClient(ecfg)
 	if err != nil {
-		t.Errorf("error creating second client: %v", err)
+		t.Fatalf("error creating second client: %v", err)
 	}
-	lc := make(chan string)
+	lc := make(chan string, 1)
 	t.Run("link client 1", func(t *testing.T) {
+		t.Parallel()
 		lh := &linkHandlerTest{desc: "client1", linkChan: lc}
 		err := client1.LinkGen(lh)
 		if err != nil {
-			t.Fatalf("failed to link in go routine: %v", err)
+			t.Fatalf("failed to link client 1: %v", err)
 		}
 	})
-	tok := <-lc
-	lh := &linkHandlerTest{desc: "client1", linkChan: lc}
-	err = client2.Link(lh, tok)
-	if err != nil {
-		t.Fatalf("failed to link in go routine: %v", err)
-	}
+	t.Run("link client 2", func(t *testing.T) {
+		t.Parallel()
+		tok := <-lc
+		lh := &linkHandlerTest{desc: "client2", linkChan: lc}
+		err = client2.Link(lh, tok)
+		if err != nil {
+			t.Fatalf("failed to link client 2: %v", err)
+		}
+	})
 }
 
 type linkHandlerTest struct {
