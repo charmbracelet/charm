655ceb85e540948bf9d2c3ca99e042aeb43db624
bashbunni <bashbunni.io@gmail.com>
2022-07-06 16:39:15 -0700

    add newline after path

--- cmd/where.go
+++ cmd/where.go
@@ -7,16 +7,16 @@ import (
 )
 
 var WhereCmd = &cobra.Command{
-	Use: "where",
+	Use:   "where",
 	Short: "Find where your cloud.charm.sh folder resides on your machine",
-	Long: paragraph("Find the absolute path to your charm keys, databases, etc."),
+	Long:  paragraph("Find the absolute path to your charm keys, databases, etc."),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		cc := initCharmClient()
 		path, err := cc.DataPath()
 		if err != nil {
 			return err
 		}
-		fmt.Fprint(cmd.OutOrStdout(), path)
+		fmt.Fprintln(cmd.OutOrStdout(), path)
 		return nil
 	},
 }
