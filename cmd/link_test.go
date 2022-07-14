29ea044066d68ba632c58636147caa5d7a8608bb
bashbunni <bashbunni.io@gmail.com>
2022-07-14 13:04:16 -0700

    test: rejected linking request

--- cmd/link_test.go
+++ cmd/link_test.go
@@ -9,21 +9,18 @@ import (
 	"github.com/charmbracelet/charm/testserver"
 )
 
-// TestLinkGen
-func TestLinkGen(t *testing.T) {
+// TestValidLinkGen
+func TestValidLinkGen(t *testing.T) {
 	client1 := testserver.SetupTestServer(t)
-	ecfg, err := client.ConfigFromEnv()
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-	client2, err := client.NewClient(ecfg)
+	client2, err := client.NewClientWithDefaults()
 	if err != nil {
 		t.Fatalf("error creating second client: %v", err)
 	}
 	lc := make(chan string, 1)
 	t.Run("link client 1", func(t *testing.T) {
 		t.Parallel()
-		lh := &linkHandlerTest{desc: "client1", linkChan: lc}
+		lh := &linkHandlerTest{desc: "client1", linkChan: lc, approve: true}
+		// pass testing.t to it, assert error
 		err := client1.LinkGen(lh)
 		if err != nil {
 			t.Fatalf("failed to link client 1: %v", err)
@@ -40,9 +37,52 @@ func TestLinkGen(t *testing.T) {
 	})
 }
 
+// TestInvalidLinkGen
+func TestInvalidLinkGen(t *testing.T) {
+	client1 := testserver.SetupTestServer(t)
+	client2, err := client.NewClientWithDefaults()
+	if err != nil {
+		t.Fatalf("error creating second client: %v", err)
+	}
+	lc := make(chan string, 1)
+	t.Run("link client 1", func(t *testing.T) {
+		t.Parallel()
+		lh := &linkHandlerTest{desc: "client1", linkChan: lc, approve: false}
+		// pass testing.t to it, assert error
+		err := client1.LinkGen(lh)
+		if err != nil {
+			t.Fatalf("failed to link client 1: %v", err)
+		}
+	})
+	t.Run("link client 2", func(t *testing.T) {
+		t.Parallel()
+		tok := <-lc
+		lh := &linkHandlerTest{desc: "client2", linkChan: lc}
+		err = client2.Link(lh, tok)
+		if err != nil {
+			t.Fatalf("failed to link client 2: %v", err)
+		}
+		if lh.status != requestDenied {
+			t.Fatalf("expected request denied, got: %d", lh.status)
+		}
+	})
+}
+
+// use these status codes for assertions in tests
+type statusCode uint
+
+const (
+	timedout statusCode = iota
+	ok
+	invalidToken
+	requestDenied
+)
+
 type linkHandlerTest struct {
 	desc     string
 	linkChan chan string
+	approve  bool
+	status   statusCode
 }
 
 func (lh *linkHandlerTest) TokenCreated(l *proto.Link) {
@@ -60,16 +100,15 @@ func (lh *linkHandlerTest) ValidToken(l *proto.Link) {
 }
 
 func (lh *linkHandlerTest) InvalidToken(l *proto.Link) {
-	lh.printDebug("invalid token", l)
+	lh.status = invalidToken
 }
 
 func (lh *linkHandlerTest) Request(l *proto.Link) bool {
-	lh.printDebug("request", l)
-	return true
+	return lh.approve
 }
 
 func (lh *linkHandlerTest) RequestDenied(l *proto.Link) {
-	lh.printDebug("request denied", l)
+	lh.status = requestDenied
 }
 
 func (lh *linkHandlerTest) SameUser(l *proto.Link) {
@@ -78,10 +117,11 @@ func (lh *linkHandlerTest) SameUser(l *proto.Link) {
 
 func (lh *linkHandlerTest) Success(l *proto.Link) {
 	lh.printDebug("success", l)
+	lh.status = ok
 }
 
 func (lh *linkHandlerTest) Timeout(l *proto.Link) {
-	lh.printDebug("timeout", l)
+	lh.status = timedout
 }
 
 func (lh linkHandlerTest) Error(l *proto.Link) {
